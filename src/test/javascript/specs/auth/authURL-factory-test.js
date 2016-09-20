2895d33a7d002290882f9027f2a2cc1f9efcb2bf
Nick Reid <nickreid@nickreid.com>
2016-09-20 14:03:25 -0700

    OLMIS-1040: Added authService code

---
+++ src/test/javascript/specs/auth/authURL-factory-test.js
@@ -0,0 +1,45 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2013 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe("AuthURL", function () {
+
+  beforeEach(module('openlmis'));
+
+  var AuthURL;
+
+  beforeEach(inject(function (_AuthURL_) {
+    AuthURL = _AuthURL_;
+  }));
+
+  it('should return string', function () {
+    var url = AuthURL("/someURL");
+
+    expect(typeof(url)).toBe("string");
+  });
+
+  it("should format relative and absolute urls the same", function(){
+    var relativeURL = AuthURL("someURL");
+    var absoluteURL = AuthURL("/someURL");
+
+    expect(relativeURL).toEqual(absoluteURL);
+  });
+
+  it("should use AuthService URL, if set", function(){
+
+  });
+
+  it("should use OpenLMIS URL, if AuthService URL not set", function(){
+
+  });
+
+  it("should use fallback to absolute URL, if OpenLMIS URL or AuthService URL are not set", function(){
+
+  });
+});
