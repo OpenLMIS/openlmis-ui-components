0fc8174417bc05b6f913b831a53f369f54a7acf6
Nick Reid <nickreid@nickreid.com>
2016-09-23 16:10:29 -0700

    OLMIS-1030: Put styles in vendor.css and app.css
    Changed build process to create 2 primary CSS files, one that pulls together vendor
    files, while the other is created by compiling app.css. There are still
    additional CSS files being created.
    
    HTML pages for index, login, and RnR were updated for new styles.
    Style guide has styles updated also.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,21 +21,27 @@ module.exports = function(grunt) {
       all: [config.app.src + '/webapp/public/js/**/*.js']
     },
     sasslint: {
-      src: [config.app.src + '/webapp/public/scss/*.scss', '!**/ng-grid.scss'],
+      src: [
+        config.app.src + '/webapp/public/scss/*.scss',
+        '!**/ng-grid.scss'
+      ],
       options: {
         bench: false,
         config: '.sasslint.json'
       }
     },
     sass: {
+      options: {
+        sourceMap: true,
+      },
       compile: {
         files: [{
           expand: true,
-          cwd: config.app.src + '/webapp/public/scss/',
-          src: ["**/mixins.scss", "**/*.scss"],
+          cwd: config.app.src + '/webapp/public/scss',
+          src: ["*.scss"],
           dest: config.app.dest + '/public/css',
           ext: ".css",
-          flatten: false
+          flatten: true
         }]
       }
     },
@@ -54,9 +60,9 @@ module.exports = function(grunt) {
         preserveComments: false
       },
       files: {
-        cwd: config.app.src + '/webapp/public/js/',
+        cwd: config.app.dest + '/public/',
         src: ['**/*.js'],
-        dest: config.app.dest + '/public/js',
+        dest: config.app.dest + '/public/',
         expand: true,
         flatten: false
       }
@@ -127,6 +133,15 @@ module.exports = function(grunt) {
           // NEED file to declare openlmis-app
         ],
         dest: config.app.dest + '/public/openlmis.js'
+      },
+      vendorCss: {
+        src: [
+          config.app.src + '/webapp/public/lib/bootstrap/css/bootstrap.min.css',
+          config.app.src + '/webapp/public/lib/angular-ui/angular-ui.min.css',
+          config.app.src + '/webapp/public/lib/select2/select2.css',
+          config.app.src + '/webapp/public/css/ng-grid.css',
+        ],
+        dest: config.app.dest + '/public/css/vendor.css'
       }
     },
     copy: {
@@ -151,6 +166,17 @@ module.exports = function(grunt) {
           }
         ],
       },
+      img: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            cwd: config.app.src + '/webapp/public/lib/',
+            src: ['**/img/**'],
+            dest: config.app.dest + '/public/img'
+          }
+        ],
+      },
       fonts: {
         files: [
           {
@@ -168,16 +194,6 @@ module.exports = function(grunt) {
             cwd: config.app.src + '/webapp/public/pages/',
             src: ['**'],
             dest: config.app.dest + '/public/pages'
-          }
-        ],
-      },
-      lib: {
-        files: [
-          {
-            expand: true,
-            cwd: config.app.src + '/webapp/public/lib/',
-            src: ['**'],
-            dest: config.app.dest + '/public/lib'
           }
         ],
       },
@@ -204,12 +220,12 @@ module.exports = function(grunt) {
     },
     gulp: {
       'styleguide-generate': function() {
-        return gulp.src([ config.app.dest + "/public/css/base.css",
-                          config.app.dest + "/public/css/content.css",
-                          config.app.dest + "/public/css/header.css",
-                          config.app.dest + "/public/css/navigation.css",
-                          config.app.dest + "/public/css/font-awesome/icons.css",
-                          config.app.dest + "/public/body.css"])
+        return gulp.src([ config.app.src + "/webapp/public/scss/base.scss",
+                          config.app.src + "/webapp/public/scss/content.scss",
+                          config.app.src + "/webapp/public/scss/header.scss",
+                          config.app.src + "/webapp/public/scss/navigation.scss",
+                          config.app.src + "/webapp/public/scss/font-awesome/icons.scss",
+                        ])
           .pipe(styleguide.generate({
             title: 'OpenLMIS Styleguide',
             rootPath: outputPath,
@@ -221,22 +237,28 @@ module.exports = function(grunt) {
           .pipe(gulp.dest(outputPath));
       },
       'styleguide-applystyles': function() {
-        return gulp.src([ config.app.dest + "/public/lib/bootstrap/css/bootstrap.min.css",
+        return gulp.src([ config.app.dest + "/public/css/vendor.css",
                    config.app.dest + "/public/css/app.css",
-                   config.app.dest + "/public/lib/select2/select2.css",
-                   config.app.dest + "/public/lib/select2/select2.png" ])
+                   config.app.dest + "/public/body.css",
+                  ])
           .pipe(styleguide.applyStyles())
           .pipe(gulp.dest(outputPath));
       },
+      'styleguide-fonts': function() {
+        return gulp.src([
+            config.app.src + "/webapp/public/fonts/*",
+          ])
+          .pipe(gulp.dest(outputPath + "/fonts"))
+      },
       'styleguide-png': function() {
-        return gulp.src([ config.app.dest + "/public/images/tab-error.png",
-                   config.app.dest + "/public/images/close-icon.png" ])
-          .pipe(gulp.dest("images"));
+        return gulp.src([ config.app.dest + "/public/images/*",
+                   config.app.dest + "/public/images/*" ])
+          .pipe(gulp.dest(outputPath + "/images"));
       }
     }
   });
 
   grunt.registerTask('build', ['clean', 'copy', 'concat', 'sass', 'uglify', 'replace', 'karma']);
   grunt.registerTask('check', ['clean', 'jshint', 'sasslint']);
-  grunt.registerTask('styleguide', ['gulp:styleguide-generate', 'gulp:styleguide-png', 'gulp:styleguide-applystyles']);
+  grunt.registerTask('styleguide', ['gulp:styleguide-generate', 'gulp:styleguide-png', 'gulp:styleguide-fonts', 'gulp:styleguide-applystyles']);
 };
