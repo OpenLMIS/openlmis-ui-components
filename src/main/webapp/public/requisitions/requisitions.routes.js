3e077c7684943ed7fa8f22d37be3821e0eda44b7
Mateusz Kwiatkowski <1stmateusz@gmail.com>
2016-10-21 07:28:07 +0200

    OLMIS-1004: move init-rnr state to main requisition route file

--- src/main/webapp/public/requisitions/requisitions.routes.js
+++ src/main/webapp/public/requisitions/requisitions.routes.js
@@ -32,6 +32,26 @@
 				}
       		}
     	});
+
+    	$stateProvider.state('requisitions.initRnr', {
+			url: '/initialize',
+			controller: 'InitiateRnrController',
+			templateUrl: 'requisitions/init.html',
+			resolve: {
+				facility: function (AuthorizationService, $q) {
+				  	var deferred = $q.defer();
+
+					AuthorizationService.getDetailedUser().$promise.then(function(response) {
+					    deferred.resolve(response.homeFacility);
+				    }, function(response) {
+				      	alert('Cannot find user info');
+				      	deferred.reject();
+				  	});
+
+					return deferred.promise;
+				}
+      		}
+		});
 	}
 
 })();
