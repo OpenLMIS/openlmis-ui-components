4be6e6ebeb8fb386e046b45c5881dbf2c8abc80c
Mateusz Kwiatkowski <1stmateusz@gmail.com>
2016-10-21 10:16:40 +0200

    OLMIS-1075: make approved quantity and remarks column editable for authorized requisition

--- src/main/webapp/public/requisitions/product-grid/product-grid-cell.directive.js
+++ src/main/webapp/public/requisitions/product-grid/product-grid-cell.directive.js
@@ -17,19 +17,36 @@
     return directive;
 
     function link(scope, element) {
+      var editable = isEditable(scope);
       $q.all([
-        $templateRequest('requisitions/product-grid/' + ((scope.column.source === 'USER_INPUT' && scope.ngModel.status !== "AUTHORIZED") ? 'user-input' : 'read-only') + '-cell.html'),
+        $templateRequest('requisitions/product-grid/' + (editable ? 'user-input' : 'read-only') + '-cell.html'),
         $templateRequest('requisitions/product-grid/product-grid-cell-error.html')
       ]).then(
         function(templates) {
           var input = angular.element(templates[0]);
-          if (scope.column.columnDefinition.columnType === 'NUMERIC' && scope.column.source === 'USER_INPUT' && scope.ngModel.status !== "AUTHORIZED") {
+          if (scope.column.columnDefinition.columnType === 'NUMERIC' && scope.column.source === 'USER_INPUT' && editable) {
             input.attr('positive-integer', '');
           }
           element.append($compile(input)(scope));
           element.append($compile(templates[1])(scope));
         }
       );
+    }
+
+    function isEditable(scope) {
+      var editable = scope.column.source === 'USER_INPUT' ? true : false;
+      switch (scope.ngModel.status) {
+        case "AUTHORIZED":
+          if (scope.column.name === "approvedQuantity" || scope.column.name === "remarks") {
+            editable = true;
+          } else {
+            editable = false;
+          }
+          break;
+        case "APPROVED":
+          editable = false;
+      }
+      return editable;
     }
   }
 
