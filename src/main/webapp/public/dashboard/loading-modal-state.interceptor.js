f73c227bc1dad84c16aa63bc7a37f4dcb8527a81
Nick Reid <nickreid@nickreid.com>
2016-11-07 13:01:42 -0800

    OLMIS-1160: Added pattern for catching ui-router state updates

--- src/main/webapp/public/dashboard/loading-modal-state.interceptor.js
+++ src/main/webapp/public/dashboard/loading-modal-state.interceptor.js
@@ -9,30 +9,26 @@
     function stateInterceptorListeners($rootScope, $timeout, LoadingModal){
 
         $rootScope.$on('$stateChangeStart', createModal);
-        //$rootScope.$on('$stateChangeSuccess', closeModal);
-        //$rootScope.$on('$stateChangeError', closeModal);
-        //$rootScope.$on('$stateNotFound', closeModal);
+        $rootScope.$on('$stateChageSuccess', closeModal);
+        $rootScope.$on('$stateChageError', closeModal);
+        $rootScope.$on('$stateNotFound', closeModal);
 
         var timeoutPromise;
 
         function createModal(){
-            console.log("state change - create");
             // pause timeout so modal doesn't open immedately all the damn time
- //           timeoutPromise = $timeout(function(){
+            timeoutPromise = $timeout(function(){
                 // open modal
-                LoadingModal.startLoading();
                 timeoutPromise = null;
- //           }, 500);
+            }, 500);
         }
 
         function closeModal(){
-            console.log("state change - close");
             if(timeoutPromise){
                 // didn't have chance to open modal, cancel opening it
                 $timeout.cancel(timeoutPromise)
                 timeoutPromise = null;
             } else {
-                LoadingModal.stopLoading();
                 // close loading modal...
             }
         }
