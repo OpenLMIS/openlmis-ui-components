67a199e08f4d160f3bfcb57933d5fd3746036ad8
Nick Reid <nickreid@nickreid.com>
2016-11-08 10:33:01 -0800

    OLMIS-1256: Added documentation to AuthorizationService

--- src/main/webapp/public/common/authorization-service.js
+++ src/main/webapp/public/common/authorization-service.js
@@ -1,15 +1,15 @@
-/*
- * This program is part of the OpenLMIS logistics management information system platform software.
- * Copyright © 2013 VillageReach
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
- */
 
  (function(){
   "use strict";
+
+  /**
+   * @ngdoc service
+   * @name openlmis-common.AuthorizationService
+   *
+   * @description
+   * This service is responsible for storing user authentication details, such as the current user's authorization rights and user object. This service only stores information, other services and factories are responsible for writing user information to the AuthorizationService. 
+   * 
+   */
 
   angular.module('openlmis-core')
     .service('AuthorizationService', AuthorizationService)
@@ -46,6 +46,16 @@
 
     var clientId, clientSecret;
 
+    /**
+     * @ngdoc function
+     * @name  getAccessToken
+     * @methodOf openlmis-common.AuthorizationService
+     *
+     * @description Retrives the current access token or returns false
+     *
+     * @returns {string} Current access token or false.
+     * 
+     */
     function getAccessToken(){
       var access_token = localStorageService.get(storageKeys.ACCESS_TOKEN);
       if(access_token){
@@ -55,14 +65,40 @@
       }
     }
 
+    /**
+     *
+     * @ngdoc function
+     * @name  setAccessToken
+     * @methodOf openlmis-common.AuthorizationService
+     * @param {string} value Value to save as the access token
+     *
+     * @description Sets the access token
+     *
+     * @returns {boolean} Returns true if value successfully saved
+     * 
+     */
     function setAccessToken(value){
       return localStorageService.add(storageKeys.ACCESS_TOKEN, value);
     }
 
+    /**
+     * 
+     * @ngdoc function
+     * @name  clearAccessToken
+     * @methodOf openlmis-common.AuthorizationService
+     * @return {boolean} Returns true if access token was cleared
+     * 
+     */
     function clearAccessToken(){
       return localStorageService.remove(storageKeys.ACCESS_TOKEN);
     }
 
+    /**
+     * @ngdoc function
+     * @name  isAuthenticated
+     * @methodOf openlmis-common.AuthorizationService
+     * @return {Boolean} Returns true if the current user is authenticated
+     */
     function isAuthenticated(){;
         if(getAccessToken()){
             return true;
@@ -71,6 +107,12 @@
         }
     }
 
+    /**
+     * @ngdoc function
+     * @name getUser
+     * @methodOf openlmis-common.AuthorizationService
+     * @return {Object} Returns at object with the current user's username and user_id
+     */
     function getUser(){
       return {
         username: localStorageService.get(storageKeys.USERNAME),
@@ -83,16 +125,49 @@
       return $injector.get('UserFactory').get(user.user_id);
     }
 
+    /**
+     * @ngdoc function
+     * @name  getUser
+     * @methodOf openlmis-common.AuthorizationService
+     * 
+     * @param {String} username Username for the current user
+     * @param {String} user_id User ID for the current user
+     *
+     * @returns {Boolean} Returns true is user credentials successfully set
+     *
+     * @description Sets the user's name and password in local storages
+     * 
+     */
     function setUser(user_id, username){
-      localStorageService.add(storageKeys.USERNAME, username);
-      localStorageService.add(storageKeys.USER_ID, user_id);
+      if( localStorageService.add(storageKeys.USERNAME, username) &&
+        localStorageService.add(storageKeys.USER_ID, user_id)){
+        return true;
+      } else {
+        return false;
+      }
     }
 
+    /**
+     * @ngdoc function
+     * @name clearUser
+     * @methodOf openlmis-common.AuthorizationService
+     * 
+     * @returns {Boolean} Returns true if user details successfully cleared
+     */
     function clearUser(){
       localStorageService.remove(storageKeys.USERNAME);
       localStorageService.remove(storageKeys.USER_ID);
     }
 
+    /**
+     * @ngdoc function
+     * @name  getRights
+     * @methodOf openlmis-common.AuthorizationService
+     *
+     * @description Reads local storages to set a list of the user's rights
+     * 
+     * @return {Array} List of the user's rights
+     */
     function getRights(){
       var rights = false;
       try{
@@ -104,12 +179,26 @@
       return rights;
     }
 
+    /**
+     * @ngdoc function
+     * @name  setRights
+     * @methodOf openlmis-common.AuthorizationService
+     * 
+     * @param {Array} An array of the user's rights
+     */
     function setRights(rights){
       if(!rights) rights = defaultRights;
       var rightsJson = JSON.stringify(rights);
       localStorageService.add(storageKeys.USER_RIGHTS, rightsJson);
     }
 
+    /**
+     * @ngdoc function
+     * @name  clearRights
+     * @methodOf openlmis-common.AuthorizationService
+     *
+     * @description Removes user rights from local storage
+     */
     function clearRights(){
       localStorageService.remove(storageKeys.USER_RIGHTS); 
     }
