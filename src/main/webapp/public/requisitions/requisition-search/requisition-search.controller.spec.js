97911e4b0fbbce671c3c5f83e5d0513959f9ee1c
Mateusz Kwiatkowski <1stmateusz@gmail.com>
2016-11-08 08:54:09 +0100

    OLMIS-1220: remove commented code and remove diasabling unit tests

--- src/main/webapp/public/requisitions/requisition-search/requisition-search.controller.spec.js
+++ src/main/webapp/public/requisitions/requisition-search/requisition-search.controller.spec.js
@@ -8,7 +8,7 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-ddescribe('RequisitionSearchController', function() {
+describe('RequisitionSearchController', function() {
 
     var scope, ctrl, httpBackend, controller, statuses, endDate, startDate
         facilityList = [
@@ -61,10 +61,6 @@ ddescribe('RequisitionSearchController', function() {
         startDate = new Date();
         endDate = new Date();
 
-        // httpBackend.when('GET', RequisitionURL('/api/requisitions/search?endDate=' + endDate.toISOString() + '&facility=' + facilityList[0].id + 
-        //     '&program=' + facilityList[0].supportedPrograms[0].id + '&startDate=' + startDate.toISOString() + 
-        //     '&status%5B%5D=' + statuses[1].label + '&status%5B%5D=' + statuses[2].label))
-        // .respond(200, requisitionList);
         ctrl = controller('RequisitionSearchController', {$scope:scope, facilityList:facilityList});
     }));
 
