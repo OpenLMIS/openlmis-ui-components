f22af94ef5e522d3f0914b24960c6d95caac162b
Nick Reid <nickreid@nickreid.com>
2016-11-08 09:16:07 -0800

    added documentation for openlmis-auth module

--- src/main/webapp/public/auth/auth.module.js
+++ src/main/webapp/public/auth/auth.module.js
@@ -6,6 +6,9 @@
     *
     * @module openlmis-auth
     *
+    * @description
+    * The openlmis-auth module is responsible for logging a user in and out of the OpenLMIS-UI, and managing authentication when making calls to other services.
+    *
     */
 
     angular.module('openlmis-auth', [
@@ -32,6 +35,14 @@
         });
     }
 
+    /**
+     * @ngdoc function
+     * @name  setIsAuthenticated
+     * @memberOf openlmis-auth
+     *
+     * @description When user authentication changes, $rootScope.userIsAuthenticated is updated for convenience.
+     * 
+     */
     setRootValues.$inject = ['$rootScope', 'AuthorizationService'];
     function setRootValues($rootScope, AuthorizationService){
         $rootScope.$watch(function(){
