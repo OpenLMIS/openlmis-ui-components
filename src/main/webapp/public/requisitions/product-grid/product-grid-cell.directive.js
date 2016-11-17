b16503f5f9bccc7050a3c8073405d85eb3afdc8c
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-11-17 20:23:38 +0100

    OLMIS-1260: Documented the productGridCell directive

--- src/main/webapp/public/requisitions/product-grid/product-grid-cell.directive.js
+++ src/main/webapp/public/requisitions/product-grid/product-grid-cell.directive.js
@@ -1,69 +1,83 @@
 (function() {
-  
-  'use strict';
+	
+	'use strict';
 
-  angular
-    .module('openlmis.requisitions')
-    .directive('productGridCell', productGridCell);
+	/**
+	 * @ngdoc directive
+	 * @name openlmis.requisitions.productGridCell
+	 *
+	 * @description
+	 * Responsible for rendering the product grid cell based on the column source and requisitions type.
+	 * It also keeps track of the validation as well as changes made to the related cells.
+	 *
+	 * @example
+	 * Here we extend our product grid cell with the directive.
+	 * ```
+	 * <td ng-repeat="column in visibleColumns | orderBy : 'displayOrder'" product-grid-cell></td>
+	 * ```
+	 */
+	angular
+		.module('openlmis.requisitions')
+		.directive('productGridCell', productGridCell);
 
-  productGridCell.$inject = ['$q', '$templateRequest', '$compile', 'Column', 'Source', 'Type'];
+	productGridCell.$inject = ['$q', '$templateRequest', '$compile', 'Column', 'Source', 'Type'];
 
-  function productGridCell($q, $templateRequest, $compile, Column, Source, Type) {
-    var directive = {
-      restrict: 'A',
-      require: '^productGrid',
-      link: link
-    };
-    return directive;
+	function productGridCell($q, $templateRequest, $compile, Column, Source, Type) {
+		var directive = {
+			restrict: 'A',
+			require: '^productGrid',
+			link: link
+		};
+		return directive;
 
-    function link(scope, element) {
-      var requisition = scope.ngModel,
-          column = scope.column;
+		function link(scope, element) {
+			var requisition = scope.ngModel,
+					column = scope.column;
 
-      scope.isReadOnly = isReadOnly();
-      scope.validate = validate;
+			scope.isReadOnly = isReadOnly();
+			scope.validate = validate;
 
-      $q.all([
-        $templateRequest('requisitions/product-grid/product-grid-cell.html'),
-        $templateRequest('requisitions/product-grid/product-grid-adjustment-cell.html')
-      ]).then(
-          function (templates) {
-            if (!isLossesAndAdjustmentCell(scope) || scope.isReadOnly) {
-              var cell = angular.element(templates[0]);
-              if (column.type === Type.NUMERIC && !scope.isReadOnly) {
-                cell.find('input').attr('positive-integer', '');
-              }
-              element.append($compile(cell)(scope));
-            } else {
-              element.append($compile(templates[1])(scope));
-            }
-          }
-      );
+			$q.all([
+				$templateRequest('requisitions/product-grid/product-grid-cell.html'),
+				$templateRequest('requisitions/product-grid/product-grid-adjustment-cell.html')
+			]).then(
+				function (templates) {
+					if (!isLossesAndAdjustmentCell(scope) || scope.isReadOnly) {
+						var cell = angular.element(templates[0]);
+						if (column.type === Type.NUMERIC && !scope.isReadOnly) {
+							cell.find('input').attr('positive-integer', '');
+						}
+						element.append($compile(cell)(scope));
+					} else {
+						element.append($compile(templates[1])(scope));
+					}
+				}
+			);
 
-      angular.forEach(column.dependencies, function(dependency) {
-        scope.$watch('lineItem.' + dependency, function(newValue, oldValue) {
-          if (newValue !== oldValue) {
-            validate();
-          }
-        });
-      });
+			angular.forEach(column.dependencies, function(dependency) {
+				scope.$watch('lineItem.' + dependency, function(newValue, oldValue) {
+					if (newValue !== oldValue) {
+						validate();
+					}
+				});
+			});
 
-      function validate() {
-        scope.lineItem.$isColumnValid(column, scope.columns);
-      }
+			function validate() {
+				scope.lineItem.$isColumnValid(column, scope.columns);
+			}
 
-      function isReadOnly() {
-        if (requisition.$isApproved()) return true;
-        if (requisition.$isAuthorized()) {
-          return [Column.APPROVED_QUANTITY, Column.REMARKS].indexOf(column.name) === -1;
-        }
-        return column.source !== Source.USER_INPUT;
-      }
+			function isReadOnly() {
+				if (requisition.$isApproved()) return true;
+				if (requisition.$isAuthorized()) {
+					return [Column.APPROVED_QUANTITY, Column.REMARKS].indexOf(column.name) === -1;
+				}
+				return column.source !== Source.USER_INPUT;
+			}
 
-      function isLossesAndAdjustmentCell(scope) {
-        return scope.column.name === 'totalLossesAndAdjustments' ? true : false;
-      }
-    }
-  }
+			function isLossesAndAdjustmentCell(scope) {
+				return scope.column.name === 'totalLossesAndAdjustments' ? true : false;
+			}
+		}
+	}
 
 })();
