a73ff648a7bb7ae6733b4401e6cc0672918cd95c
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-11-17 20:14:56 +0100

    OLMIS-1260: Documented the positiveInteger directive

--- src/main/webapp/public/requisitions/product-grid/positive-integer.directive.js
+++ src/main/webapp/public/requisitions/product-grid/positive-integer.directive.js
@@ -1,30 +1,43 @@
 (function() {
 
-  'use strict';
+	'use strict';
 
-  angular
-    .module('openlmis.requisitions')
-    .directive('positiveInteger', positiveInteger);
+	/**
+	 * @ngdoc directive
+	 * @name openlmis.requisitions.positiveInteger
+	 *
+	 * @description
+	 * Restricts the ngModel to only allow positive interegers.
+	 *
+	 * @example
+	 * Extend the input element to force it to only accept positive integers as values.
+	 * ```
+	 * <input ng-model="someModel" positive-integer>
+	 * ```
+	 */
+	angular
+		.module('openlmis.requisitions')
+		.directive('positiveInteger', positiveInteger);
 
-  function positiveInteger() {
-    var directive = {
-      require: 'ngModel',
-      link: link
-    };
-    return directive;
+	function positiveInteger() {
+		var directive = {
+			require: 'ngModel',
+			link: link
+		};
+		return directive;
 
-    function link(scope, element, attrs, modelCtrl) {
-      modelCtrl.$parsers.push(function (inputValue) {
+		function link(scope, element, attrs, modelCtrl) {
+			modelCtrl.$parsers.push(function (inputValue) {
 
-        if (inputValue == undefined) return '' 
-        var transformedInput = inputValue.replace(/[^0-9]/g, ''); 
-        if (transformedInput!=inputValue) {
-          modelCtrl.$setViewValue(transformedInput);
-          modelCtrl.$render();
-        }         
+				if (inputValue == undefined) return '' 
+				var transformedInput = inputValue.replace(/[^0-9]/g, ''); 
+				if (transformedInput!=inputValue) {
+					modelCtrl.$setViewValue(transformedInput);
+					modelCtrl.$render();
+				}         
 
-        return transformedInput ? parseInt(transformedInput) : null;
-      });
-    }
-  }
+				return transformedInput ? parseInt(transformedInput) : null;
+			});
+		}
+	}
 })();
