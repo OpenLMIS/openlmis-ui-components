47aa56b659b7ef666622ef5b6fb3b972d3e9c627
przemyslawStudzinskiSolDevelo <pstudzinski@soldevelo.com>
2016-11-21 16:22:07 +0100

    OLMIS-1261 Document Approve Requisitions
    add documentation to ApprovalListController

--- src/main/webapp/public/requisitions/approval-list/approval-list.controller.js
+++ src/main/webapp/public/requisitions/approval-list/approval-list.controller.js
@@ -2,6 +2,14 @@
 
 	'use strict';
 
+    /**
+     * @ngdoc controller
+     * @name openlmis.requisitions.RequisitionApprovalListController
+     *
+     * @description
+     * Controller for approval list of requisitions.
+     */
+
 	angular.module('openlmis.requisitions')
 		.controller('ApprovalListCtrl', ApprovalListCtrl);
 
@@ -9,10 +17,44 @@
 
 	function ApprovalListCtrl($scope, $state, requisitionList, $location, messageService, DateUtils) {
 
+        /**
+         * @ngdoc property
+         * @name requisitions
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds currently requisitions.
+         */
 		$scope.requisitions = requisitionList;
+
+        /**
+         * @ngdoc property
+         * @name filteredRequisitions
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds currently filtered requisitions using parameters.
+         */
 		$scope.filteredRequisitions = $scope.requisitions;
+
+        /**
+         * @ngdoc property
+         * @name selectedItems
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds currently filtered requisitions. Using query to filter.
+         */
 		$scope.selectedItems = [];
 
+        /**
+         * @ngdoc property
+         * @name labels
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds need labels.
+         */
 	 	$scope.labels = [
 			{
 				value: '',
@@ -35,6 +77,14 @@
 			}
 		}, true);
 
+        /**
+         * @ngdoc property
+         * @name gridOptions
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds configuration of the grid.
+         */
 		$scope.gridOptions = { data: 'filteredRequisitions',
 			showFooter: false,
 			showSelectionCheckbox: false,
@@ -83,12 +133,31 @@
 			]
 		};
 
+
+        /**
+         * @ngdoc property
+         * @name openRnr
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds currently handler to redirect to requisition page after clicking on grid row.
+         *
+         */
 		$scope.openRnr = function (row) {
 			$state.go('requisitions.requisition', {
 				rnr: row.entity.id
 			});
 		};
 
+        /**
+         * @ngdoc property
+         * @name filterRequisitions
+         * @propertyOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Holds currently filtered requisitions. Using filter value and search in specific field.
+         *
+         */
 		$scope.filterRequisitions = function () {
 			$scope.filteredRequisitions = [];
 			var query = $scope.query || "";
@@ -102,16 +171,54 @@
 			$scope.resultCount = $scope.filteredRequisitions.length;
 		};
 
+        /**
+         * @ngdoc function
+         * @name getFieldValue
+         * @methodOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Get value of field from object using dot char to split it.
+         *
+         * @param  {Object} object    the object to get value
+         * @param  {String} fieldName the name of field
+         * @return {String}           value of field
+         */
 		function getFieldValue(object, fieldName) {
 			return fieldName.split('.').reduce(function(a, b) {
 				return a[b];
 			}, object);
 		}
 
+        /**
+         * @ngdoc function
+         * @name contains
+         * @methodOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Check if string contains specific substring.
+         * If substring exist in string return index of first char of substring.
+         *
+         * @param  {String} string the string wherein search
+         * @param  {String} query  the substring to search
+         * @return {number}        index in string table of specific first char of substring
+         */
 		function contains(string, query) {
 			return string.toLowerCase().indexOf(query.toLowerCase()) != -1;
 		}
 
+        /**
+         * @ngdoc function
+         * @name matchesAnyField
+         * @methodOf openlmis.requisitions.RequisitionApprovalListController
+         *
+         * @description
+         * Concat strings: program name, facility name and code to one string. Check if this string
+         * contains specific substring. If substring exist in string return index of first char of substring.
+         *
+         * @param  {String} query the string to search
+         * @param  {Object} rnr   the requisition to get program name facility name and code
+         * @return {number}       index in string table of specific first char of substring
+         */
 		function matchesAnyField(query, rnr) {
 			var rnrString = "|" + rnr.program.name + "|" + rnr.facility.name + "|" + rnr.facility.code + "|";
 			return contains(rnrString, query);
