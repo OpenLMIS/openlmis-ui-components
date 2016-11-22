e2709d053886e2b431e1c7c1490663660bab36d1
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-11-22 16:45:04 +0100

    OLMIS-1209: Added column.spec.js

---
+++ src/main/webapp/public/requisitions/column.spec.js
@@ -0,0 +1,136 @@
+describe('Column', function() {
+
+    var Colum, Status, Columns, Source, Type;
+
+    var columnDef, requisition;
+
+    beforeEach(module('openlmis.requisitions'));
+
+    beforeEach(inject(function(_Column_, _Status_, _Columns_, _Source_, _Type_) {
+        Column = _Column_;
+        Status = _Status_;
+        Columns = _Columns_;
+        Source = _Source_;
+        Type = _Type_;
+    }));
+
+    beforeEach(function() {
+        columnDef = {
+            name: Columns.STOCK_ON_HAND,
+            source: Source.CALCULATED,
+            label: 'Stock on Hand',
+            isDisplayed: true,
+            displayOrder: 1,
+            columnDefinition: {
+                columnType: Type.NUMERIC
+            }
+        };
+        requisition = {
+            status: Status.SUBMITTED
+        };
+    });
+
+    it('should create Column from definition', function() {
+        var column = new Column(columnDef, requisition);
+
+        expect(column.name).toBe(Columns.STOCK_ON_HAND);
+        expect(column.source).toBe(Source.CALCULATED);
+        expect(column.type).toBe(Type.NUMERIC);
+        expect(column.label).toBe('Stock on Hand');
+        expect(column.display).toBe(true);
+        expect(column.displayOrder).toBe(1);
+        expect(column.required).toBe(false);
+        expect(column.fullSupplyOnly).toBe(true);
+        expect(column.dependencies).toEqual([
+            Columns.BEGINNING_BALANCE,
+            Columns.TOTAL_RECEIVED_QUANTITY,
+            Columns.TOTAL_CONSUMED_QUANTITY,
+            Columns.TOTAL_LOSSES_AND_ADJUSTMENTS
+        ]);
+    });
+
+    it('should hide column if it is invisible', function() {
+        columnDef.isDisplayed = false;
+
+        var column = new Column(columnDef, requisition);
+
+        expect(column.display).toBe(false);
+    });
+
+    [
+        {
+            name: 'should be required if it is mandatory and user input',
+            column: 'stockOnHand',
+            source: 'USER_INPUT',
+            result: true
+        }, {
+            name: 'should not be required if it is not mandatory',
+            column: 'remarks',
+            source: 'USER_INPUT',
+            result: false
+        }, {
+            name: 'should not be required if it mandatory but not use input',
+            column: 'stockOnHand',
+            source: 'CALCULATED',
+            result: false
+        }
+    ].forEach(function(testCase) {
+        it(testCase.name, function() {
+            columnDef.name = testCase.column;
+            columnDef.source = testCase.source;
+
+            var column = new Column(columnDef, requisition);
+
+            expect(column.required).toBe(testCase.result);
+        });
+    });
+
+    [
+        {
+            name: 'should hide Approved Quantity column if status is not authorizde/approved',
+            column: 'approvedQuantity',
+            status: 'SUBMITTED',
+            result: false
+        },
+        {
+            name: 'should hide Remarks column if status is not authorizde/approved',
+            column: 'remarks',
+            status: 'SUBMITTED',
+            result: false
+        },
+        {
+            name: 'should show Approved Quantity column if status is authorized',
+            column: 'approvedQuantity',
+            status: 'AUTHORIZED',
+            result: true
+        },
+        {
+            name: 'should show Approved Quantity column if status is approved',
+            column: 'approvedQuantity',
+            status: 'APPROVED',
+            result: true
+        },
+        {
+            name: 'should show Remarks column if status is authorized',
+            column: 'remarks',
+            status: 'AUTHORIZED',
+            result: true
+        },
+        {
+            name: 'should show Remarks column if status is authorized',
+            column: 'remarks',
+            status: 'APPROVED',
+            result: true
+        }
+    ].forEach(function(testCase) {
+        it(testCase.name, function() {
+            columnDef.name = testCase.column;
+            requisition.status = testCase.status;
+
+            var column = new Column(columnDef, requisition);
+
+            expect(column.display).toBe(testCase.result);
+        });
+    });
+
+});
