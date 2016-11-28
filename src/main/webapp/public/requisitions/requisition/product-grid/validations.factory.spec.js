9b81fccca001a324ee9c5a155e4f0a7f525624ec
Nick Reid <nickreid@nickreid.com>
2016-11-28 12:36:57 -0800

    OLMIS-1287: Added tests for functions that return functions

--- src/main/webapp/public/requisitions/requisition/product-grid/validations.factory.spec.js
+++ src/main/webapp/public/requisitions/requisition/product-grid/validations.factory.spec.js
@@ -1,5 +1,5 @@
 
-describe('validations', function() {
+ddescribe('validations', function() {
 
     var validations, messageService;
 
@@ -8,11 +8,14 @@ describe('validations', function() {
     beforeEach(inject(function(_validations_, _messageService_) {
         validations = _validations_;
         messageService = _messageService_;
+
+        spyOn(messageService, 'get').andCallFake(function(str){
+            return str;
+        });
     }));
 
     it('should return "negative" error message when value is negative', function() {
 
-        spyOn(messageService, 'get');
         validations.nonNegative(-1);
         expect(messageService.get).toHaveBeenCalledWith('error.negative');
 
@@ -20,7 +23,6 @@ describe('validations', function() {
 
     it('should return "required" error message when value is blank', function() {
 
-        spyOn(messageService, 'get');
         validations.nonEmpty("");
         expect(messageService.get).toHaveBeenCalledWith('error.required');
 
@@ -28,7 +30,6 @@ describe('validations', function() {
 
     it('should pass validation when value is non negative', function() {
 
-        spyOn(messageService, 'get');
         validations.nonNegative(0);
         expect(messageService.get).not.toHaveBeenCalled();
 
@@ -36,10 +37,44 @@ describe('validations', function() {
 
     it('should pass validation when value is not empty', function() {
 
-        spyOn(messageService, 'get');
         validations.nonEmpty(2);
         expect(messageService.get).not.toHaveBeenCalled();
 
+    });
+
+    it('validates if method set property is non-empty', function(){
+
+        var checkPropertyFoo = validations.nonEmptyIfPropertyIsSet('foo');
+
+        expect(checkPropertyFoo("",{})).toBe(undefined);
+        expect(checkPropertyFoo("bar",{foo:"something"})).toBe(undefined);
+        expect(checkPropertyFoo("",{foo:"something"})).toBe('error.required');
+
+        var checkPropertyBar = validations.nonEmptyIfPropertyIsSet('bar');
+        expect(checkPropertyBar("",{})).toBe(undefined);
+        expect(checkPropertyBar("foo",{bar:"something"})).toBe(undefined);
+        expect(checkPropertyBar("",{bar:"something"})).toBe('error.required');
+    });
+
+    it('validates a calculation was performed correctly', function(){
+        // will take a calculation
+        function addFooToBar(obj){
+            return obj.foo + obj.bar; 
+        }
+
+        var checkCalculationFooBar = validations.validateCalculation(addFooToBar);
+
+        var item = {
+            foo: 1,
+            bar: 1
+        };
+
+        expect(checkCalculationFooBar(2, item)).toBe(undefined);
+        expect(checkCalculationFooBar(7, item)).toBe('error.wrongCalculation');
+
+        item.bar = 2;
+        expect(checkCalculationFooBar(2, item)).toBe('error.wrongCalculation');        
+        expect(checkCalculationFooBar(3, item)).toBe(undefined);
     });
 
 });
