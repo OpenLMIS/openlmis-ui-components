cb222d7e30e3d404c4ee981b9c3f0b32831e2e15
Nick Reid <nickreid@nickreid.com>
2016-11-30 14:56:26 -0800

    added back in magic juju

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # Sync with Transifex
 ./sync_transifex.sh
 
+# Not 100% sure how this helps
+npm install -g bower
+rm -r ./bower_components
+
 # Update everything (just in case)
 bower install --allow-root
 npm install --no-optional
