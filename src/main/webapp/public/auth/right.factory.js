d09c72e770d6f6e5da97f55ffdbb3fd91626360e
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-12-05 20:10:34 +0100

    OLMIS-1373: Added JSDoc to the right.factory.js file

--- src/main/webapp/public/auth/right.factory.js
+++ src/main/webapp/public/auth/right.factory.js
@@ -2,6 +2,13 @@
 
     'use strict';
 
+    /**
+     * @ngdoc service
+     * @name openlmis-auth.Right
+     *
+     * @description
+     * Represents a single right along with related programs, warehouses and supervisory nodes.
+     */
     angular
         .module('openlmis-auth')
         .factory('Right', factory);
@@ -14,6 +21,19 @@
 
         return Right;
 
+        /**
+         * @ngdoc function
+         * @methodOf openlmis-auth.Right
+         * @name constructor
+         *
+         * @description
+         * Creates a Right class object.
+         *
+         * @param {String}  name        the name of the Right
+         * @param {Array}   programs    the list of related programs
+         * @param {Array}   facilities  the list of related warehouses
+         * @param {Array}   nodes       the list of related supervisory nodes
+         */
         function Right(name, programs, facilities, nodes) {
             this.name = name;
             this.programs = programs;
@@ -21,6 +41,17 @@
             this.nodes = nodes;
         }
 
+        /**
+         * @ngdoc methodOf
+         * @methodOf openlmis-auth.Right
+         * @name buildRights
+         *
+         * @description
+         * Builds a list or rights from the given list of role assignments.
+         *
+         * @param  {Array}  assignments the list of role assignments
+         * @return {Array}              the list of user rights
+         */
         function buildRights(assignments) {
             var rights = {};
 
