28a5749b8aadd299aa90917598bc8497266e35c8
Weronika Ciecierska <wciecierska@soldevelo.com>
2016-12-06 16:56:40 +0100

    OLMIS-1233: Added unit tests for Convert to order modal service.
    Added two jasmine tests for new service added in scope of OLMIS-1233.

---
+++ src/main/webapp/public/requisitions/requisition/convert-to-order-modal/convert-to-order-modal.service.spec.js
@@ -0,0 +1,74 @@
+describe("ConvertToOrderModal", function(){
+
+    var convertToOrderModal, ngBootbox, rootScope, requisitionService, q, state;
+    var type = {
+        name: 'facilityType'
+    },
+    facility = {
+        id: '1',
+        name: 'facility',
+        type: type
+    },
+    program = {
+        id: '1',
+        name: 'program'
+    },
+    requisition = {
+        id: '1',
+        name: 'requisition',
+        status: 'INITIATED',
+        supplyingFacility: supplyingDepotSpy(0),
+        program: program,
+        facility: facility
+    },
+    requisitions = [{
+        requisition: requisition,
+        supplyingDepots: [
+        supplyingDepotSpy(0),
+        supplyingDepotSpy(1),
+        supplyingDepotSpy(2),
+        supplyingDepotSpy(3)
+    ]}];
+
+    beforeEach(module('openlmis.requisitions'));
+
+    beforeEach(inject(function(_ConvertToOrderModal_, _$ngBootbox_, _$rootScope_,
+    _RequisitionService_, _$q_){
+        convertToOrderModal = _ConvertToOrderModal_;
+        ngBootbox = _$ngBootbox_;
+        rootScope = _$rootScope_;
+        requisitionService = _RequisitionService_;
+        q = _$q_;
+    }));
+
+    it('should open modal for convert requisition to order', function(){
+        var result;
+        spyOn(requisitionService, 'forConvert').andReturn(q.when(requisitions));
+        spyOn(ngBootbox, 'customDialog');
+
+        convertToOrderModal.show(requisition).then(function(data) {
+            result = data;
+        });
+        rootScope.$apply();
+
+        expect(ngBootbox.customDialog).toHaveBeenCalled();
+        expect(result).toEqual(requisitions[0]);
+    });
+
+    it('should close modal for convert requisition for order', function(){
+        spyOn(requisitionService, 'forConvert').andReturn(q.when(requisitions));
+        spyOn(ngBootbox, 'customDialog');
+        spyOn(ngBootbox, 'hideAll');
+
+        convertToOrderModal.show(requisition);
+        convertToOrderModal.close();
+
+        expect(ngBootbox.hideAll).toHaveBeenCalled();
+    });
+
+    function supplyingDepotSpy(id) {
+        return {
+            $id: id
+        };
+    }
+});
