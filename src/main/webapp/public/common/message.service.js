c7ec88dcdaa76b1bbe9333d429df48e63871ae95
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-12-12 14:09:11 +0100

    Made MessageService use  instead of

--- src/main/webapp/public/common/message.service.js
+++ src/main/webapp/public/common/message.service.js
@@ -1,43 +1,50 @@
+(function() {
+    "use strict";
 
-(function(){
-  "use strict";
+    angular.module('openlmis-core')
+        .factory('messageService', messageService);
 
-  angular.module('openlmis-core')
-    .factory('messageService', messageService);
+    messageService.$inject = ['localStorageService', '$rootScope', 'version', '$resource'];
 
-  messageService.$inject = ['localStorageService', '$rootScope', 'version', '$http'];
+    function messageService(localStorageService, $rootScope, version, $resource) {
 
-  function messageService (localStorageService, $rootScope, version, $http) {
-
-    var populate = function (selectedLocale) {
-      if (localStorageService.get('version') != version) {
-        localStorageService.add('version', version);
-        $http.get('/public/messages/messages_' + selectedLocale + '.json').success(function (data) {
-          for (var attr in data) {
-            localStorageService.add('message.' + attr, data[attr]);
-          }
-          $rootScope.$broadcast('messagesPopulated');
+        var resource = $resource('/public/messages/messages_:locale.json', {
+            locale: '@locale'
         });
-      }
-    };
 
-    var get = function () {
-      var keyWithArgs = Array.prototype.slice.call(arguments);
-      var displayMessage =  localStorageService.get('message.' + keyWithArgs[0]);
-      if(keyWithArgs.length > 1 && displayMessage) {
-        $.each(keyWithArgs, function (index, arg) {
-          if (index > 0) {
-            displayMessage = displayMessage.replace("{" + (index-1) + "}", arg);
-          }
-        });
-      }
-      return displayMessage || keyWithArgs[0];
-    };
+        var populate = function(selectedLocale) {
+            if (localStorageService.get('version') != version) {
+                localStorageService.add('version', version);
+                resource.get({
+                    locale: selectedLocale
+                }).$promise.then(function(data) {
+                    for (var attr in data) {
+                        localStorageService.add('message.' + attr, data[attr]);
+                    }
+                    $rootScope.$broadcast('messagesPopulated');
+                }, function() {
+                    console.log('fdgdgfd');
+                });
+            }
+        };
 
-    return{
-      populate:populate,
-      get:get
-    };
-  }
+        var get = function() {
+            var keyWithArgs = Array.prototype.slice.call(arguments);
+            var displayMessage = localStorageService.get('message.' + keyWithArgs[0]);
+            if (keyWithArgs.length > 1 && displayMessage) {
+                $.each(keyWithArgs, function(index, arg) {
+                    if (index > 0) {
+                        displayMessage = displayMessage.replace("{" + (index - 1) + "}", arg);
+                    }
+                });
+            }
+            return displayMessage || keyWithArgs[0];
+        };
+
+        return {
+            populate: populate,
+            get: get
+        };
+    }
 
 })();
