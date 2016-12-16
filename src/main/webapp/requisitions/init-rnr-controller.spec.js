fba363c6e290e2240a55081821427b15e9676cda
Weronika Ciecierska <wciecierska@soldevelo.com>
2016-12-16 13:39:32 +0100

    OLMIS-1377: Added init-rnr-controller tests
    Added four new tests that test funcionality added in scope of ticket OLMIS-1377.
    Also refactored other tests.

--- src/main/webapp/requisitions/init-rnr-controller.spec.js
+++ src/main/webapp/requisitions/init-rnr-controller.spec.js
@@ -1,52 +1,48 @@
 describe("InitiateRnrController", function(){
 
-    var $q, scope, ctrl, $httpBackend, location, facilities, programs, rootScope,
-    navigateBackService, periodFactory, $state, period;
+    var $q, programs, rootScope, requisitionService, authorizationService, supervisedFacilities,
+        periodFactory, $state, period, facility;
 
     beforeEach(module('openlmis.requisitions'));
     beforeEach(inject(function (_$q_, $rootScope, $controller, _PeriodFactory_,
-    _$state_, _RequisitionService_) {
+    _$state_, _RequisitionService_, _AuthorizationService_, _SupervisedFacilities_) {
+
         rootScope = $rootScope;
-        scope = $rootScope.$new();
         periodFactory =_PeriodFactory_;
         $state = _$state_;
         requisitionService = _RequisitionService_;
+        authorizationService = _AuthorizationService_;
+        supervisedFacilities = _SupervisedFacilities_;
         $q = _$q_;
 
         user = {"user_id": "user_id"};
         programs = [{item: {"code": "HIV", "id": 1}}];
+        period = [{"id": 1, "rnrId": 123, "startDate": "01-01-2016", "endDate": "02-02-2016"}];
         facility = {
             "id": "10134",
             "name": "National Warehouse",
             "description": null,
             "code": "CODE",
-            "supportedPrograms": programs};
+            "supportedPrograms": programs
+        };
 
-        
-        initController = function() {
-            return $controller('InitiateRnrController', {facility: facility, user: user, supervisedPrograms: [],
-            PeriodFactory: periodFactory, RequisitionService: requisitionService});
-        }
-
-        period = [{"id": 1, "rnrId": 123, "startDate": "01-01-2016", "endDate": "02-02-2016"}];
         spyOn(periodFactory, 'get').andReturn($q.when(period));
+
+        vm = $controller('InitiateRnrController', {facility: facility, user: user, supervisedPrograms: programs,
+            PeriodFactory: periodFactory, RequisitionService: requisitionService});
     }));
 
     it("should assign proper values when facility is assigned", function() {
-        var controller = initController();
-
-        expect(controller.selectedFacilityId).toEqual(facility.id);
-        expect(controller.programs).toEqual(programs);
-        expect(controller.selectedProgram).toEqual(programs[0]);
+        expect(vm.selectedFacilityId).toEqual(facility.id);
+        expect(vm.programs).toEqual(programs);
+        expect(vm.selectedProgram).toEqual(programs[0]);
     });
 
     it("Should change page to requisitions.requisition for with selected period with rnrId", function(){
-        var controller = initController();
         var selectedPeriod = {"rnrId": 1};
-
         spyOn($state, 'go');
 
-        controller.initRnr(selectedPeriod);
+        vm.initRnr(selectedPeriod);
 
         expect($state.go).toHaveBeenCalledWith('requisitions.requisition.fullSupply', {"rnr":1});
 
@@ -55,14 +51,11 @@ describe("InitiateRnrController", function(){
     it("Should change page to requisitions.requisition for newly initialized requisition in selected period",
     function(){
         var selectedPeriod = {"id":1};
-
         spyOn($state, 'go');
         spyOn(requisitionService, 'initiate').andReturn($q.when({"id": 1}));
 
-        var controller = initController();
-
-        controller.initRnr(selectedPeriod);
-        scope.$apply();
+        vm.initRnr(selectedPeriod);
+        rootScope.$apply();
 
         expect($state.go).toHaveBeenCalledWith('requisitions.requisition.fullSupply', {"rnr": 1});
     });
@@ -70,27 +63,55 @@ describe("InitiateRnrController", function(){
     it("Should not change page to requisitions.requisition with selected period without rnrId "
     + "and when invalid response from service",
     function(){
-        spyOn(requisitionService,'initiate').andReturn($q.reject({"id": 1}));
-
         var selectedPeriod = {};
-
+        spyOn(requisitionService,'initiate').andReturn($q.reject({"id": 1}));
         spyOn($state, 'go');
 
-        var controller = initController();
-
-        controller.initRnr(selectedPeriod);
-        scope.$apply();
+        vm.initRnr(selectedPeriod);
+        rootScope.$apply();
 
         expect($state.go).not.toHaveBeenCalled();
     });
 
     it("Should reload periods with proper data", function() {
-
-        var controller = initController();
-        controller.loadPeriods();
-        scope.$apply();
+        vm.loadPeriods();
+        rootScope.$apply();
 
         expect(periodFactory.get).toHaveBeenCalled();
-        expect(controller.periodGridData).toEqual(period);
+        expect(vm.periodGridData).toEqual(period);
     });
+
+    it("should load proper data for supervised facility", function() {
+        vm.loadFacilityData(true);
+
+        expect(vm.facilities).toEqual([]);
+        expect(vm.programs).toEqual(vm.supervisedPrograms);
+        expect(vm.selectedFacilityId).toEqual(undefined);
+    });
+
+    it("should load proper data for home facility", function() {
+        vm.loadFacilityData(false);
+
+        expect(vm.facilities).toEqual([facility]);
+        expect(vm.programs).toEqual(vm.homeFacilityPrograms);
+        expect(vm.selectedFacilityId).toEqual(facility.id);
+    });
+
+    it("should load list of facilities for selected program", function() {
+        var spyObj = {};
+        spyObj.spyMethod = supervisedFacilities;
+        spyOn(spyObj, 'spyMethod').andReturn([facility]);
+        spyOn(authorizationService, 'getRightIdByName').andReturn("rightId");
+
+        vm.loadFacilities(vm.supervisedPrograms[0]);
+
+        expect(vm.facilities).toEqual([facility]);
+    });
+
+    it("should return empty list of facilities for undefined program", function() {
+        vm.loadFacilities(undefined);
+
+        expect(vm.facilities).toEqual([]);
+    });
+
 });
