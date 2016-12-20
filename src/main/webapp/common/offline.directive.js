33fdf651f7df161e4bb64d3f8383b676dd18bae9
Nick Reid <nickreid@nickreid.com>
2016-12-19 19:44:19 -0800

    OLMIS-1406: Updated documentation for offline directive

--- src/main/webapp/common/offline.directive.js
+++ src/main/webapp/common/offline.directive.js
@@ -1,13 +1,18 @@
 (function() {
-
     'use strict';
 
     /**
      * @ngdoc directive
      * @name openlmis-core.directive:offline
+     * @restrict A
      *
      * @description
      * Directive for determining if user is online or not.
+     *
+     * @example
+     * ```
+     * <button offline ng-disabled="isOffline">Do something</button>
+     * ```
      */
 
     angular
@@ -18,28 +23,40 @@
 
     function offline(OfflineService) {
         var directive = {
+            restrict: 'A',
             scope: false,
             replace: false,
             link: link
         }
         return directive;
 
-        /**
-         * @ngdoc function
-         * @name link
-         * @methodOf openlmis-core.directive:offline
-         *
-         * @description
-         * Watches status of connection and updates value in scope.
-         *
-         */
         function link(scope, element, attr) {
+
+            /**
+             * @ngdoc property
+             * @name isOffline
+             * @propertyOf openlmis-core.directive:offline
+             * @type {Boolean}
+             *
+             * @description
+             * A boolean that says if there is an internet connection, as
+             * determined by the offlineService.
+             */
             scope.$watch(function(){
                 return OfflineService.isOffline();
             }, function(isOffline) {
                 scope.isOffline = isOffline;
             }, true);
 
+            /**
+             * @ngdoc property
+             * @name checkConnection
+             * @propertyOf openlmis-core.directive:offline
+             * @type {Boolean}
+             *
+             * @description
+             * Makes the offlineService check if there is a connection to the internet.
+             */
             scope.checkConnection = function() {
                 return OfflineService.checkConnection();
             };
