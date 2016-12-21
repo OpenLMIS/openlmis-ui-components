46cfa8570e29194272cc4b69f2cec186e4fcc6fb
Nick Reid <nickreid@nickreid.com>
2016-12-20 22:05:02 -0800

    OLMIS-1349: Directive to place table sort order correctly

---
+++ src/main/webapp/tables/table-sort-order.directive.js
@@ -0,0 +1,51 @@
+(function() {
+
+	'use strict';
+
+	/**
+	 * @ngdoc directive
+	 * @name openlmis.table:table-sort-order
+	 *
+	 * @description
+	 * Positions the table sort order correctly in a table's container.
+	 *
+	 * @example
+	 * ```
+	 * <div class="table-container">
+	 * <form table-sort-order>
+	 * 	 <label for="table-sort-order">Sort items by</label>
+	 *   <select id="table-sort-order"> 
+	 *     <option selected="selected">Name</option>
+	 *   </select>
+	 *   <input type="submit" value="Sort items" />
+	 * </form>
+	 * <table>
+	 *   <tbody title="Category Title">
+	 *     <tr><td>123</td><td>456</td></tr>
+	 *     <tr><td>Foo</td><td>Bar</td></tr>
+	 *   </tbody>
+	 * </table>
+	 * </div>
+	 * ```
+	 */
+	angular
+		.module('openlmis-table')
+		.directive('tableSortOrder', directive);
+
+	directive.$inject = [];
+	function directive() {
+    	return {
+      		restrict: 'AC',
+			replace: false,
+      		link: link
+    	};
+    	function link(scope, element, attrs) {
+    		element.addClass('table-sort-order');
+    		var parent = angular.element(element.parent()[0]);
+    		if(parent.hasClass('table-container')){
+    			parent.css('padding-top', element.outerHeight());
+    		}
+    	}
+	}
+
+})();
