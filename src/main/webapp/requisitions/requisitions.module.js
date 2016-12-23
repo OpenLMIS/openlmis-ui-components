2a0749333b5e8969e116f59744bb4270654414e8
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-12-23 14:47:15 +0100

    OLMIS-1469: Added missing dependency to the openlmis.requisitions module

--- src/main/webapp/requisitions/requisitions.module.js
+++ src/main/webapp/requisitions/requisitions.module.js
@@ -4,6 +4,7 @@
 
     angular.module('openlmis.requisitions', [
         'requisition-full-supply',
+        'requisition-non-full-supply',
         'angular.filter',
         'openlmis-core',
         'openlmis-templates',
