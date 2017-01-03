2801a12be0bccaa1e67ddbeadc8521d48007aebd
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2017-01-03 15:05:55 +0100

    OLMIS-1569: Substract 1 from number of month, because in js months are from 0 to 11

--- src/main/webapp/openlmis-date/date-utils.factory.js
+++ src/main/webapp/openlmis-date/date-utils.factory.js
@@ -16,7 +16,9 @@
         function toDate(array) {
             if (!array) return undefined;
             if (array.length === 3) return new Date(array.toString());
-            if (array.length === 6) return new Date(array[0], array[1], array[2], array[3], array[4], array[5]);
+            if (array.length === 6)
+                // array[1] - 1, because in JavaScript months starts with 0 (to 11)
+                return new Date(array[0], array[1] - 1, array[2], array[3], array[4], array[5]);
             return undefined;
         }
     }
