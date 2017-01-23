0f6ecd41b90336f91766cafdff9edd210cbb202d
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-01-23 10:43:05 +0100

    OLMIS-1549: Added formNovalidate directive

---
+++ src/main/webapp/openlmis-form/form-novalidate.directive.js
@@ -0,0 +1,30 @@
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @name openlmis-form.form
+     *
+     * @description
+     * Dynamically adds novalidate directive to the form.
+     */
+    angular
+        .module('openlmis-form')
+        .directive('form', directive);
+
+    function directive() {
+        var directive = {
+            link: link,
+            restrict: 'E'
+        };
+        return directive;
+    }
+
+    function link(scope, element, attrs) {
+        if (!attrs.hasOwnProperty('novalidate')) {
+            element.attr('novalidate', '');
+        }
+    }
+
+})();
