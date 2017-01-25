39ed9b7b6d18cac58a4c9419c953a52b9482ac3a
Nick Reid <nickreid@nickreid.com>
2017-01-25 12:32:42 -0800

    OLMIS-1072: Simplified workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,3 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('openlmis-ui');
-
-    grunt.option('applicationDirectories', [
-        '/openlmis/openlmis-requisition-ui'
-        ]);
 };
