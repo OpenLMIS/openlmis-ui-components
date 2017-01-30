33b36a90c632709e9bc1f1bc82b6c3b43f4eef95
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-01-30 19:15:07 +0100

    turned back on all tests

--- src/main/webapp/openlmis-i18n/message.service.spec.js
+++ src/main/webapp/openlmis-i18n/message.service.spec.js
@@ -8,7 +8,7 @@
 * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
 */
 
-ddescribe("MessageService", function () {
+describe("MessageService", function () {
     var $rootScope, messageService;
     beforeEach(function(){
 
