42f10e3ae4400190e33b399ede90d4ba08e24665
Nick Reid <nickreid@nickreid.com>
2017-01-31 14:13:29 -0800

    OLMIS-1072: Updated docker-compose build paths

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@
 ./sync_transifex.sh
 
 # Update everything (just in case)
-bower install --allow-root --config.interactive=false
 npm rebuild
 npm install --no-optional
 
 # Built and test
-grunt build
+grunt
