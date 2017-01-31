3033d5f51108b7bbca8681c4c0d8cd24561d4d1f
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-01-31 21:28:17 +0100

    OLMIS-1601: Added missing JSDocs to the RequisitionWatcher

--- src/main/webapp/requisition-view/requisition-watcher.factory.js
+++ src/main/webapp/requisition-view/requisition-watcher.factory.js
@@ -2,6 +2,14 @@
 
     'use strict';
 
+    /**
+     * @ngdoc service
+     * @name requisition-view.RequisitionWatcher
+     *
+     * @description
+     * Provides auto-save feature to the requisition. Notifies when changes are being made to the
+     * watched requisition - this can be avoided by silencing the watcher.
+     */
     angular
         .module('requisition-view')
         .factory('RequisitionWatcher', factory);
@@ -9,6 +17,7 @@
     factory.$inject = ['$rootScope', '$timeout', 'notificationService', 'localStorageFactory'];
 
     function factory($rootScope, $timeout, notificationService, localStorageFactory) {
+
         RequisitionWatcher.prototype.makeSilent = makeSilent;
         RequisitionWatcher.prototype.makeLoud = makeLoud;
 
@@ -37,12 +46,30 @@
             }, true);
         }
 
+        /**
+         * @ngdoc method
+         * @methodOf requisition-view.RequisitionWatcher
+         * @name makeSilent
+         *
+         * @description
+         * Makes the watcher silent - no notification will be displayed after this method has been
+         * called.
+         */
         function makeSilent() {
             var watcher = this;
             $timeout.cancel(watcher.notificationTimeout);
             watcher.isLoud = false;
         }
 
+        /**
+         * @ngdoc method
+         * @methodOf requisition-view.RequisitionWatcher
+         * @name makeLoud
+         *
+         * @description
+         * Makes the watcher loud - notification will be shown once in a while when editing the
+         * watched requisition.
+         */
         function makeLoud() {
             this.isLoud = true;
         }
