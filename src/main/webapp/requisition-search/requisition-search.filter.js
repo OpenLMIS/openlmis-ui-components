0b30f0d6c89a506030face41bd9d37f5f31fe538
Chongsun Ahn <cahn922@yahoo.com>
2017-02-08 21:35:20 -0800

    OLMIS-1788 Rename createdDate to initiatedDate
    Matches API endpoint signature.

--- src/main/webapp/requisition-search/requisition-search.filter.js
+++ src/main/webapp/requisition-search/requisition-search.filter.js
@@ -27,8 +27,8 @@
 
                 if(params.program && params.program != requisition.program.id) match = false;
                 if(params.facility && params.facility != requisition.facility.id) match = false;
-                if(params.createdDateFrom && new Date(params.createdDateFrom) > new Date(createdDate)) match = false;
-                if(params.createdDateTo && new Date(params.createdDateTo) < new Date(createdDate)) match = false;
+                if(params.initiatedDateFrom && new Date(params.initiatedDateFrom) > new Date(createdDate)) match = false;
+                if(params.initiatedDateTo && new Date(params.initiatedDateTo) < new Date(createdDate)) match = false;
                 if(params.emergency && params.emergency != requisition.emergency) match = false;
                 if(params.requisitionStatus && !matchStatus(requisition.status, params.requisitionStatus)) match = false;
 
