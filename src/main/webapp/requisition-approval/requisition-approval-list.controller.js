8d108b1d6f95166be760941a6c2ca805f9c04ff8
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-02-15 14:01:21 +0100

    OLMIS-1354: Removed sort controls from the approve screen

--- src/main/webapp/requisition-approval/requisition-approval-list.controller.js
+++ src/main/webapp/requisition-approval/requisition-approval-list.controller.js
@@ -21,7 +21,6 @@
 		var vm = this;
 
 		vm.openRnr = openRnr;
-		vm.changeSortOrder = changeSortOrder;
 
         /**
          * @ngdoc property
@@ -33,68 +32,6 @@
          * Holds requisitions.
          */
 		vm.requisitions = requisitionList;
-
-		/**
-         * @ngdoc property
-         * @name columns
-         * @propertyOf requisition-approval.RequisitionApprovalListController
-         * @type {Array}
-         *
-         * @description
-         * Holds available columns for sorting.
-         */
-		vm.columns = [
-			{
-				name: 'program.name',
-				label: messageService.get('label.program')
-			},
-			{
-				name: 'facility.code',
-				label: messageService.get('label.facilityCode')
-			},
-			{
-				name: 'facility.name',
-				label: messageService.get('label.facilityName')
-			},
-			{
-				name: 'facility.type.name',
-				label: messageService.get('label.facilityType')
-			}
-		];
-
-		/**
-		 * @ngdoc property
-		 * @name selectedColumn
-		 * @propertyOf requisition-approval.RequisitionApprovalListController
-		 * @type {Array}
-		 *
-		 * @description
-		 * Holds current column that items are sorted by.
-		 */
-		vm.selectedColumn = vm.columns[0].name;
-
-		/**
-		 * @ngdoc property
-		 * @name reverse
-		 * @propertyOf requisition-approval.RequisitionApprovalListController
-		 * @type {Array}
-		 *
-		 * @description
-		 * True if items are sorted descending.
-		 */
-		vm.reverse = true;
-
-		/**
-         * @ngdoc method
-         * @name changeSortOrder
-         * @propertyOf requisition-approval.RequisitionApprovalListController
-         *
-         * @description
-         * Changes sort order (ascending/descending).
-         */
-		function changeSortOrder() {
-			vm.reverse = !vm.reverse;
-		}
 
         /**
          * @ngdoc property
