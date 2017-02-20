186f7c4bc1dadd92d1c772506e356a0e1c62af14
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-02-20 15:52:45 +0100

    OLMIS-1886: Fixed memory leak in loginModalInterceptor

--- src/main/webapp/openlmis-login/login-modal.interceptor.js
+++ src/main/webapp/openlmis-login/login-modal.interceptor.js
@@ -8,62 +8,76 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
  */
 
- (function(){
-    "use strict";
+(function() {
 
-    angular.module('openlmis-login')
+    'use strict';
+
+    angular
+        .module('openlmis-login')
         .run(loginRequiredInterceptor);
 
     /**
-    * @ngdoc function
-    * @name  openlmis-login.loginRequiredInterceptor
-    *
-    * @description
-    * When there is 401 unauthorized status code after request, the user is shown login modal window. After authenticate request is retried.
-    *
-    */
-    loginRequiredInterceptor.$inject = ['$rootScope', '$compile', 'bootbox', '$templateRequest', 'loadingModalService', 'authService', 'accessTokenFactory'];
-    function loginRequiredInterceptor($rootScope, $compile, bootbox, $templateRequest, loadingModalService, authService, accessTokenFactory) {
-        var noRetryRequest;
-        var dialog;
+     * @ngdoc function
+     * @name  openlmis-login.loginRequiredInterceptor
+     *
+     * @description
+     * When there is 401 unauthorized status code after request, the user is shown login modal
+     * window. After authenticate request is retried.
+     *
+     */
+    loginRequiredInterceptor.$inject = [
+        '$rootScope', '$compile', 'bootbox', '$templateRequest', 'loadingModalService',
+        'authService', 'accessTokenFactory'
+    ];
+
+    function loginRequiredInterceptor($rootScope, $compile, bootbox, $templateRequest,
+                                      loadingModalService, authService, accessTokenFactory) {
+
+        var noRetryRequest, dialog, dialogScope;
 
         $rootScope.$on('event:auth-loginRequired', onLoginRequired);
+        $rootScope.$on('auth.login-modal', onLoginModal);
 
         /**
-          *
-          * @ngdoc function
-          * @name onLoginRequired
-          * @param {Object} event event
-          * @param {boolean} noRetryRequest true if should no retry request
-          *
-          * @description
-          * Make and show login modal, close loading modal.
-          *
-          */
+         *
+         * @ngdoc function
+         * @name onLoginRequired
+         * @param {Object} event event
+         * @param {boolean} noRetryRequest true if should no retry request
+         *
+         * @description
+         * Make and show login modal, close loading modal.
+         *
+         */
         function onLoginRequired(event, _noRetryRequest_) {
-          var scope = $rootScope.$new();
-          noRetryRequest = _noRetryRequest_;
+            noRetryRequest = _noRetryRequest_;
 
-          $templateRequest('openlmis-login/login-form.html').then(function (html) {
-            dialog = bootbox.dialog({
-              message: $compile(html)(scope),
-              closeButton: false,
-              className: 'login-modal'
+            if (dialogScope) dialogScope.$destroy();
+            dialogScope = $rootScope.$new();
+
+            $templateRequest('openlmis-login/login-form.html').then(function(html) {
+                dialog = bootbox.dialog({
+                    message: $compile(html)(dialogScope),
+                    closeButton: false,
+                    className: 'login-modal'
+                });
             });
-          });
-          loadingModalService.close();
+            loadingModalService.close();
         }
 
-        $rootScope.$on('auth.login-modal', function () {
-          dialog.modal('hide');
-          if (noRetryRequest === true) {
-            $rootScope.$emit('event:auth-loggedIn');
-          } else {
-            authService.loginConfirmed(null, function (config) {
-              config.url = accessTokenFactory.updateAccessToken(config.url);
-              return config;
-            });
-          }
-        });
+        function onLoginModal() {
+            dialog.modal('hide');
+            dialog = undefined;
+            dialogScope.$destroy();
+
+            if (noRetryRequest === true) {
+                $rootScope.$emit('event:auth-loggedIn');
+            } else {
+                authService.loginConfirmed(null, function(config) {
+                    config.url = accessTokenFactory.updateAccessToken(config.url);
+                    return config;
+                });
+            }
+        }
     }
 })();
