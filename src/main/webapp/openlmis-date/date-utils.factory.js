37b26c58ef7715a1e093536a8f8875b9658b1fb3
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-02-20 17:40:02 +0900

    OLMIS-1805: added skipping timezone offset if there is no defined in ISO string

--- src/main/webapp/openlmis-date/date-utils.factory.js
+++ src/main/webapp/openlmis-date/date-utils.factory.js
@@ -19,12 +19,21 @@
 
         function toDate(array) {
             if (!array) return undefined;
-            if(!angular.isArray(array)) return new Date(array); // when date is ISO string, not array
+            if(!angular.isArray(array)) return fromISOString(array); // when date is ISO string, not array
             if (array.length === 3) return new Date(array[0], array[1] - 1, array[2]);
             if (array.length === 6)
                 // array[1] - 1, because in JavaScript months starts with 0 (to 11)
                 return new Date(array[0], array[1] - 1, array[2], array[3], array[4], array[5]);
             return undefined;
+        }
+
+        function fromISOString(isoDate) {
+            var date = new Date(isoDate);
+            if(isoDate.indexOf('Z') < 0) { // if date string does not contain time zone definition
+                var offset = date.getTimezoneOffset() * 60000; // remove time zone offset
+                date = new Date(date.getTime() + offset);
+            }
+            return date;
         }
 
         function toArray(date, includeTime) {
