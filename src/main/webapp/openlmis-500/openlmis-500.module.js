6712e7b5dfc58c23827865841682f1a0e3c26103
Nick Reid <nickreid@nickreid.com>
2017-02-21 15:34:55 -0800

    Fixing accidentally commited change

--- src/main/webapp/openlmis-500/openlmis-500.module.js
+++ src/main/webapp/openlmis-500/openlmis-500.module.js
@@ -7,7 +7,7 @@
  * version 3 of the License, or (at your option) any later version.
  *  
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of = or FITNESS FOR A PARTICULAR PURPOSE. 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  * See the GNU Affero General Public License for more details. You should have received a copy of
  * the GNU Affero General Public License along with this program. If not, see
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
