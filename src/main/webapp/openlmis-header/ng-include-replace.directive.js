cb6655a2221140ea6105767d4ff6b01f465b2d3b
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-02-22 12:45:00 +0100

    OLMIS-1809: Changed to Offline indicator to plain text and added button for checking connection
    * changed "Offline" link indicator to plain "You are offline" text
    * added "Check connection" button
    * modified ngInclude directive to replace the element with the template content
    * restyled header to use flex

---
+++ src/main/webapp/openlmis-header/ng-include-replace.directive.js
@@ -0,0 +1,45 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @name openlmis-header.ngInclude
+     *
+     * @description
+     * Modifies the ngInclude directive to replace the element with ngInclude directive attached
+     * with its content.
+     */
+    angular
+        .module('openlmis-header')
+        .directive('ngInclude', directive);
+
+    directive.$inject = [];
+
+    function directive() {
+        var directive = {
+            link: link,
+            priority: -1
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            element.replaceWith(element.children());
+        }
+    }
+
+})();
