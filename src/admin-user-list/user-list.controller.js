9cad7ac5f840d6b876c81596c97547c3718972eb
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-03-21 07:08:46 +0100

    OLMIS-2144: added admin user list screen

---
+++ src/admin-user-list/user-list.controller.js
@@ -0,0 +1,89 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+	'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name admin-user-list.controller:UsersListController
+     *
+     * @description
+     * Controller for managing user list screen.
+     */
+
+	angular
+		.module('admin-user-list')
+		.controller('UsersListController', controller);
+
+	controller.$inject = ['$state', 'users', 'confirmService'];
+
+	function controller($state, users, confirmService) {
+
+		var vm = this;
+
+		vm.goToCreateNewUser = goToCreateNewUser;
+        vm.editUser = editUser;
+        vm.resetUserPassword = resetUserPassword;
+
+        vm.items = users;
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-user-list.controller:UsersListController
+         * @name goToCreateNewUser
+         *
+         * @description
+         * Redirects to create new user page.
+         */
+		function goToCreateNewUser() {
+			//$state.go('');
+		}
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-user-list.controller:UsersListController
+         * @name editUser
+         *
+         * @description
+         * Redirects to edit user page.
+         *
+         * @param {String} userId the user UUID
+         */
+		function editUser(userId) {
+			/*$state.go('administration.user.edit', {
+                id: userId
+            });*/
+		}
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-user-list.controller:UsersListController
+         * @name editUser
+         *
+         * @description
+         * Redirects to edit user page.
+         *
+         * @param {String} userId the user UUID
+         */
+		function resetUserPassword(userId) {
+            confirmService.confirm('msg.question.confirmation.resetPassword').then(function() {
+
+            });
+		}
+	}
+
+})();
---
+++ src/referencedata-user/user.service.js
@@ -0,0 +1,78 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+
+(function(){
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name referencedata-user.userService
+     *
+     * @description
+     * Responsible for retrieving user info from the server.
+     */
+    angular
+        .module('referencedata-user')
+        .factory('userService', service);
+
+    service.$inject = ['openlmisUrlFactory', '$resource'];
+
+    function service(openlmisUrlFactory, $resource){
+
+        var resource = $resource(openlmisUrlFactory('/api/users/:id'), {}, {
+                'getAll': {
+                    url: openlmisUrlFactory('/api/users'),
+                    method: 'GET',
+                    isArray: true
+                }
+            });
+
+        return {
+            get: get,
+            getAll: getAll
+        };
+
+        /**
+         * @ngdoc method
+         * @methodOf referencedata-user.userService
+         * @name get
+         *
+         * @description
+         * Gets user by id.
+         *
+         * @param  {String}  id User UUID
+         * @return {Promise}    User info
+         */
+        function get(id) {
+            return resource.get({id: id}).$promise;
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf referencedata-user.userService
+         * @name getAll
+         *
+         * @description
+         * Gets all users.
+         *
+         * @return {Promise} Array of all users
+         */
+        function getAll() {
+            return resource.getAll().$promise;
+        }
+    }
+})();
