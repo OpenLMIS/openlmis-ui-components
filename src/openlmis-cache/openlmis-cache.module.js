89772d3a07d44c628a32d44e3427b1e56ea9352d
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-06-21 12:13:21 +0200

    OLMIS-2567: Added empty line

--- src/openlmis-cache/openlmis-cache.module.js
+++ src/openlmis-cache/openlmis-cache.module.js
@@ -12,6 +12,7 @@
  * the GNU Affero General Public License along with this program. If not, see
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
+
 (function() {
 
     'use strict';
