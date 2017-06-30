d54a9158a40913fccf81a863a374e0912f2d3a45
Nick Reid <nickreid@nickreid.com>
2017-06-30 14:31:21 -0700

    OLMIS-1693: Added uniqueIdService for ARIA

---
+++ src/openlmis-form/unique-id.service.spec.js
@@ -0,0 +1,51 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('Unique ID Service', function() {
+
+    'use strict';
+
+    var uniqueIdService;
+
+    beforeEach(module('openlmis-form'));
+
+    beforeEach(inject(function(_uniqueIdService_) {
+        uniqueIdService = _uniqueIdService_;
+    }))
+
+    it('generates a unique id that is a string', function(){
+        var id = uniqueIdService.generate();
+        expect(typeof(id)).toBe('string');
+    });
+
+    describe('makes unique ids for DOM', function(){
+        beforeEach(inject(function(shortid){
+            var randomIds = ['ABC', 'DEF', 'GHI'];
+            spyOn(shortid, 'gen').andCallFake(function(){
+                return randomIds.shift();
+            });
+        }));
+
+        it('will not return an id that exists as a current DOM element', function(){
+            var div = document.createElement('div');
+            div.id = 'ABC';
+            document.body.appendChild(div);
+
+            var id = uniqueIdService.generate();
+
+            expect(id).not.toBe('ABC');
+        });
+    });
+});
