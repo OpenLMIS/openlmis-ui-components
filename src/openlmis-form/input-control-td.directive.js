d067a84cbbb006c2d31eab4f0ad1fd6da2b8e715
Nick Reid <nickreid@nickreid.com>
2017-07-09 10:22:10 -0700

    OLMIS-2797: Added input-control directive and styles to TD element

---
+++ src/openlmis-form/input-control-td.directive.js
@@ -0,0 +1,70 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @restrict E
+     * @name openlmis-form.directive:inputControlTD
+     *
+     * @description
+     * Adds input-control class to table cells that include only a single input,
+     * and will map that inputs state to the table cell.
+     */
+    angular
+        .module('openlmis-form')
+        .directive('td', tdFormControl);
+
+    tdFormControl.$inject = ['$compile'];
+    function tdFormControl($compile) {
+        return {
+            restrict: 'E',
+            replace: false,
+            priority: 30,
+            compile: function(element, attrs){
+                return {
+                    pre: compile
+                };
+            }
+        };
+
+        function compile(scope, element, attrs) {
+            if(element[0].hasAttribute('input-control')){
+                // this stops an infinite loop
+                return ;
+            }
+
+            if(element.parents('[input-control]').length > 0){
+                return ;
+            }
+
+            var inputElement = element.children('input, select');
+            if(inputElement.length == 1) {
+                element.attr('input-control', '');
+
+                if(!element[0].hasAttribute('openlmis-invalid')){
+                    element.attr('openlmis-invalid', '');
+                }
+
+                var newElement = $compile(element.clone())(scope);
+                element.replaceWith(newElement);
+            }
+        }
+    }
+
+})();
---
+++ src/openlmis-form/input-control-td.directive.spec.js
@@ -0,0 +1,70 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+ddescribe('Input Control TD', function() {
+	var $compile, scope;
+
+	beforeEach(module('openlmis-form'));
+
+	beforeEach(inject(function(_$compile_, $rootScope, $templateCache) {
+		$compile = _$compile_;
+		scope = $rootScope.$new();		
+	}));
+
+	it('adds an input-control directive to table cell elements with a single input', function(){
+		var markup = '<table><tr><td><input /></td></tr></table>',
+			element = $compile(markup)(scope);
+		scope.$apply();
+
+		var tableCell = element.find('td');
+
+		expect(tableCell[0].hasAttribute('input-control')).toBe(true);
+		expect(tableCell[0].hasAttribute('openlmis-invalid')).toBe(true);
+
+		// make sure no child input-controls exist
+		expect(tableCell.find('[input-control]').length).toBe(0);
+	});
+
+	it('will not add input control if any parent is an input-control', function(){
+		var markup = '<div input-control><table><tr><td><input /></td></tr></table></div>',
+			element = $compile(markup)(scope);
+		scope.$apply();
+
+		var tableCell = element.find('td');
+
+		expect(tableCell[0].hasAttribute('input-control')).toBe(false);
+	});
+
+	it('will add input-control for a select element', function(){
+		var markup = '<table><tr><td><select /></td></tr></table>',
+			element = $compile(markup)(scope);
+		scope.$apply();
+
+		var tableCell = element.find('td');
+
+		expect(tableCell[0].hasAttribute('input-control')).toBe(true);
+	});
+
+	it('will not add input-control for a textarea element', function(){
+		var markup = '<table><tr><td><textarea /></td></tr></table>',
+			element = $compile(markup)(scope);
+		scope.$apply();
+
+		var tableCell = element.find('td');
+
+		expect(tableCell[0].hasAttribute('input-control')).toBe(false);
+	});
+
+});
