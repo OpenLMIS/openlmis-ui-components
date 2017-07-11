d56acee1782a1e908c837eb260645adf6a4799da
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-07-11 09:37:56 +0200

    OLMIS-2775: Work in Progress

---
+++ src/openlmis-adjustments/adjustments-modal.controller.js
@@ -0,0 +1,121 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name openlmis-adjustments.controller:AdjustmentsModalController
+     *
+     * @description
+     * Provides methods for Losses and Adjustments modal.
+     */
+    angular
+        .module('openlmis-adjustments')
+        .controller('AdjustmentsModalController', AdjustmentsModalController);
+
+    AdjustmentsModalController.$inject = [
+        '$filter', 'reasons', 'adjustments', 'title', 'message', 'totals', 'modalDeferred',
+        'getReasonName'
+    ];
+
+    function AdjustmentsModalController($filter, reasons, adjustments, title, message, totals,
+                                        modalDeferred, getReasonName) {
+
+        var vm = this;
+
+        vm.$onInit = onInit;
+        vm.addAdjustment = addAdjustment;
+        vm.removeAdjustment = removeAdjustment;
+        vm.getReasonName = getReasonName;
+        vm.hideModal = modalDeferred.reject;
+
+        /**
+         * @ngdoc property
+         * @propertyOf openlmis-adjustments.controller:AdjustmentsModalController
+         * @name adjustments
+         * @type {Array}
+         *
+         * @description
+         * Line item adjustments that will be updated.
+         */
+        vm.adjustments = undefined;
+
+        /**
+         * @ngdoc property
+         * @propertyOf openlmis-adjustments.controller:AdjustmentsModalController
+         * @name reasons
+         * @type {Array}
+         *
+         * @description
+         * Possible reasons that user can choose from.
+         */
+        vm.reasons = undefined;
+
+        /**
+         * @ngdoc methodOf
+         * @methodOf openlmis-adjustments.controller:AdjustmentsModalController
+         * @name $onInit
+         *
+         * @description
+         * Initialization method of the AdjustmentsModalController.
+         */
+        function onInit() {
+            vm.adjustments = adjustments;
+            vm.reasons = reasons;
+            vm.title = title;
+            vm.totals = totals;
+            vm.message = message;
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf openlmis-adjustments.controller:AdjustmentsModalController
+         * @name addAdjustment
+         *
+         * @description
+         * Adds adjustment to line item.
+         */
+        function addAdjustment() {
+            var adjustment = {
+                reasonId: vm.adjustment.reason.id,
+                quantity: vm.adjustment.quantity
+            };
+
+            vm.adjustments.push(adjustment);
+
+            vm.adjustment.quantity = undefined;
+            vm.adjustment.reason = undefined;
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf openlmis-adjustments.controller:AdjustmentsModalController
+         * @name removeAdjustment
+         *
+         * @description
+         * Removes adjustment to line item.
+         *
+         * @param {Object}  adjustment  the adjustment to be removed
+         */
+        function removeAdjustment(adjustment) {
+            var index = vm.adjustments.indexOf(adjustment);
+            vm.adjustments.splice(index, 1);
+        }
+    }
+
+})();
---
+++ src/openlmis-adjustments/adjustments-modal.controller.spec.js
@@ -0,0 +1,126 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('AdjustmentsModalController', function() {
+
+    var vm, $q, $controller, requisition, adjustments, reasons, lineItem, modalDeferred;
+
+    beforeEach(function() {
+        module('openlmis-adjustments');
+
+        inject(function($injector) {
+            $q = $injector.get('$q');
+            $controller = $injector.get('$controller');
+        });
+
+        adjustments = [];
+        reasons = [];
+
+        modalDeferred = $q.defer();
+
+        vm = $controller('AdjustmentsModalController', {
+            adjustments: adjustments,
+            reasons: reasons,
+            modalDeferred: modalDeferred,
+            title: 'Some title',
+            totals: undefined,
+            message: undefined,
+            getReasonName: undefined
+        });
+    });
+
+    describe('$onInit', function() {
+
+        it('should expose adjustments', function() {
+            vm.$onInit();
+
+            expect(vm.adjustments).toBe(adjustments);
+        });
+
+        it('should fetch stock adjustment reasons', function() {
+            vm.$onInit();
+
+            expect(vm.reasons).toBe(reasons);
+        });
+
+    });
+
+    describe('addAdjustment', function() {
+
+        var adjustment;
+
+        beforeEach(function() {
+            adjustment = {
+                reason: {
+                    id: 2
+                },
+                quantity: 10
+            };
+
+            vm.adjustment = angular.merge({}, adjustment);
+
+            vm.$onInit();
+        });
+
+        it('should add adjustment to stock adjustments', function() {
+            vm.addAdjustment();
+
+            expect(adjustments).toEqual([{
+                reasonId: adjustment.reason.id,
+                quantity: adjustment.quantity
+            }]);
+        });
+
+        it('should clear form after adding adjustment', function() {
+            expect(vm.adjustment.quantity).not.toBeUndefined();
+            expect(vm.adjustment.reason).not.toBeUndefined();
+
+            vm.addAdjustment();
+
+            expect(vm.adjustment.quantity).toBeUndefined();
+            expect(vm.adjustment.reason).toBeUndefined();
+        });
+
+    });
+
+    describe('removeAdjustment', function() {
+
+        var adjustment;
+
+        beforeEach(function() {
+            adjustment = {
+                reasonId: 321,
+                quantity: 11
+            };
+
+            vm.adjustments = [
+                {
+                    reasonId: 123,
+                    quantity: 10
+                },
+                adjustment
+            ];
+        });
+
+        it('should remove adjustment', function() {
+            vm.removeAdjustment(adjustment);
+
+            expect(vm.adjustments).toEqual([{
+                reasonId: 123,
+                quantity: 10
+            }]);
+        });
+    });
+});
