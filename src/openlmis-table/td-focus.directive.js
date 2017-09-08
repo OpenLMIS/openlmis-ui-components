8784084105a11f8ffa1cbf26084a6aacee251b5f
Nick Reid <nickreid@nickreid.com>
2017-09-07 19:10:24 -0700

    OLMIS-3114: Improve tableTDFocus scrolling
    - Added documentation
    - Added skeleton for unit tests

--- src/openlmis-table/td-focus.directive.js
+++ src/openlmis-table/td-focus.directive.js
@@ -20,20 +20,19 @@
     /**
      * @ngdoc directive
      * @restrict E
-     * @name openlmis-table.directive:tdFocus
+     * @name openlmis-table.directive:tableTDFocus
      *
      * @description
      * Makes sure the table cell element is focused.
-     * 
      */
     
     angular
         .module('openlmis-table')
         .directive('table', directive);
 
-    directive.$inject = ['$timeout']
+    directive.$inject = ['$timeout', '$window']
 
-    function directive($timeout) {
+    function directive($timeout, $window) {
         return {
             link: link,
             restrict: 'E'
@@ -45,26 +44,84 @@
             }
 
             element.on('focus focusin', 'td, th', checkVisibilityDeferred);
+            element.on('blur focusout', 'td, th', cancelVisibilityDeferred);
 
+            $scope.$on('$destroy', function(){
+                element.off('focus focusin', 'td, th', checkVisibilityDeferred);
+                element.off('blur focusout', 'td, th', cancelVisibilityDeferred);
+            });
+
+            /**
+             * @ngdoc method
+             * @name checkVisibilityDeferred
+             * @methodOf openlmis-table.directive:tableTDFocus
+             *
+             * @param {Event} event A DOM focus event
+             *
+             * @description
+             * Defers the action from the focus/focusin events so that multiple
+             * events execute twice.
+             */
             var checkVisibilityTimeout;
             function checkVisibilityDeferred(event) {
-                if(checkVisibilityTimeout) {
-                    $timeout.cancel(checkVisibilityTimeout);
-                }
+                cancelVisibilityDeferred();
+
                 checkVisibilityTimeout = $timeout(function() {
-                    checkVisibility(angular.element(event.target));
+                    checkVisibility(getTableCell(event));
                 }, 100);
             }
 
-            function checkVisibility(tableCell) {
-                if(tableCell.hasClass('stuck')){
-                    return;
+            /**
+             * @ngdoc method
+             * @name cancelVisibilityDeferred
+             * @methodOf openlmis-table.directive:tableTDFocus
+             *
+             * @description
+             * Cancel's checking visiblity, and is called if the table cell
+             * loses focus.
+             */
+            function cancelVisibilityDeferred() {
+                if(checkVisibilityTimeout) {
+                    $timeout.cancel(checkVisibilityTimeout);
                 }
+            }
 
-                var leftPos = tableCell.position().left,
-                    width = tableCell.outerWidth(),
-                    visibleEdgeLeft = 0,
-                    visibleEdgeRight = 0;
+            /**
+             * @ngdoc method
+             * @name getTableCell
+             * @methodOf openlmis-table.directive:tableTDFocus
+             *
+             * @param {Event} event A DOM focus event
+             *
+             * @return {Object} jQuery object for a table cell
+             *
+             * @description
+             * Checks the event's target and returns the containing table cell
+             * if the event target isn't a table cell.
+             */
+            function getTableCell(event) {
+                var nodeName = event.target.nodeName.toLowerCase();
+                if(nodeName === 'td' || nodeName === 'th') {
+                    return angular.element(event.target);
+                } else {
+                    return angular.element(event.target).parents('td, th').first();
+                }
+            }
+
+            /**
+             * @ngdoc method
+             * @name getVisibleTableArea
+             * @methodOf openlmis-table.directive:tableTDFocus
+             *
+             * @return {Object} Object with dimensions for the visible area
+             *
+             * @description
+             * Returns the visible area of the table.
+             */
+            function getVisibleTableArea() {
+                var visibleEdgeLeft = 0,
+                    visibleEdgeRight = 0,
+                    leftOffset = element.parent()[0].scrollLeft;
 
                 if(attrs.leftEdge) {
                     visibleEdgeLeft = attrs.leftEdge;
@@ -73,15 +130,60 @@
                     visibleEdgeRight = attrs.rightEdge;
                 }
 
-                if(leftPos < visibleEdgeLeft) {
-                    scrollTable(0);
-                } else if(leftPos + width > visibleEdgeRight) {
-                    scrollTable(leftPos);
+                return {
+                    leftEdge: visibleEdgeLeft,
+                    rightEdge: visibleEdgeRight,
+                    leftOffset: leftOffset
                 }
             }
 
+            /**
+             * @ngdoc method
+             * @name checkVisibility
+             * @methodOf openlmis-table.directive:tableTDFocus
+             *
+             * @param {Object} tableCell A table cell DOM element
+             *
+             * @description
+             * Check to see if the table cell is entirely inside the table
+             * element, otherwise the table is scrolled.
+             */
+            function checkVisibility(tableCell) {
+                if(tableCell.length !== 1) {
+                    return;
+                }
+
+                if(tableCell.hasClass('stuck')){
+                    return;
+                }
+
+                var visibleArea = getVisibleTableArea(),
+                    leftEdge = tableCell.position().left,
+                    width = tableCell.outerWidth(),
+                    rightEdge = visibleArea.leftOffset + leftEdge + width;
+
+                if(leftEdge < visibleArea.leftEdge || rightEdge > visibleArea.rightEdge) {
+                    scrollTable(visibleArea.leftOffset + leftEdge - visibleArea.leftEdge);
+                }
+            }
+
+            /**
+             * @ngdoc method
+             * @name scrollTable
+             * @methodOf openlmis-table.directive:tableTDFocus
+             *
+             * @param {Number} pos The position to scroll the table to
+             *
+             * @return {Object} jQuery object for a table cell
+             *
+             * @description
+             * Scrolls the table *instantly* when the next animationFrame is
+             * called.
+             */
             function scrollTable(pos) {
-                element.parent()[0].scrollLeft = pos;
+                $window.requestAnimationFrame(function() {
+                    element.parent()[0].scrollLeft = pos;
+                });
             }
         }
 
