2b333dbc497d7a6a56c9695d187d3af126dee28b
Nick Reid <nickreid@nickreid.com>
2017-09-15 16:50:02 -0700

    OLMIS-3114: Wrap checkbox inputs in table cells automatically

---
+++ src/openlmis-table-form/checkbox-input-td.directive.js
@@ -0,0 +1,66 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @restrict A
+     * @name openlmis-table-form.directive:checkboxInputTd
+     *
+     * @description
+     * Adds a label around a checkbox in a table cell, if one doesn't exist.
+     */
+    angular
+        .module('openlmis-table-form')
+        .directive('input', directive);
+
+    function directive() {
+        return {
+            link: link,
+            restrict: 'E'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            if(validTarget(element)) {
+                element.wrap('<label class="checkbox"></label>');
+            }
+        }
+
+        /**
+         * @ngdoc method
+         * @restrict A
+         * @name validTarget
+         * @methodOf openlmis-table-form.directive:checkboxInputTd
+         *
+         * @description
+         * Checks to see if a checkbox is a direct decendant of a table cell,
+         * and if so returns true.
+         */
+        function validTarget(element) {
+            var isCheckbox = element.attr('type') === 'checkbox',
+                isChildOfTD = element.parent()[0].nodeName.toLowerCase() === 'td';
+
+            if(isCheckbox && isChildOfTD) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+})();
---
+++ src/openlmis-table-form/checkbox-input-td.directive.spec.js
@@ -0,0 +1,40 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('openlmis-table-form.directive:checkboxInputTD', function() {
+    var $compile, $rootScope, input;
+
+    beforeEach(module('openlmis-table-form'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('will wrap a label element around a checkbox input directly placed in a table cell', function(){
+        var markup = '<td><input type="checkbox" /></td>'
+            element = $compile(markup)($rootScope.$new());
+
+        expect(element.find('input').parent()[0].nodeName.toLowerCase()).toBe('label');
+        expect(element.find('input').parents('label').hasClass('checkbox')).toBe(true);
+    });
+
+    it('will not add a label to an input already wrapped with a label', function(){
+        var markup = '<td><label><input type="checkbox" />Example</label></td>'
+            element = $compile(markup)($rootScope.$new());
+
+        expect(element.find('input').parents('label').length).toEqual(1);
+    });
+});
