5e9f2b5aad1292e70a4b8cda1c609e6e706a98e4
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-10-20 13:44:38 +0200

    OLMIS-3199: Removed ddescribe

--- src/openlmis-database/local-database.spec.js
+++ src/openlmis-database/local-database.spec.js
@@ -13,7 +13,7 @@
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-ddescribe('LocalDatabase', function() {
+describe('LocalDatabase', function() {
 
     var LocalDatabase, pouchDb, database;
 
