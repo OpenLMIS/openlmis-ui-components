530d8bfb1fd0f6bb23ea6e0083abdb607e100e06
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-12-13 18:05:55 +0100

    OLMIS-3195: added sonar script

---
+++ sonar.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Update everything (just in case)
+npm rebuild
+npm install --no-optional
+
+# Built and test
+grunt
+grunt sonar --sonarLogin=$SONAR_LOGIN --sonarPassword=$SONAR_PASSWORD
