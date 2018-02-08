150c22914159b0dfe6caf4e23ce3c29d47eb3238
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-02-08 17:46:18 +0100

    OLMIS-3995: Added OpenLMISRepository and OpenLMISRepositoryImpl classes

---
+++ src/openlmis-repository/openlmis-repository-impl.js
@@ -0,0 +1,82 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name openlmis-repository.OpenLMISRepositoryImpl
+     *
+     * @description
+     * Generic repository for communicating with the OpenLMIS RESTful endpoints.
+     */
+    angular
+        .module('openlmis-repository')
+        .factory('OpenLMISRepositoryImpl', OpenLMISRepositoryImpl);
+
+    OpenLMISRepositoryImpl.$inject = ['$resource', '$q'];
+
+    function OpenLMISRepositoryImpl($resource, $q) {
+
+        OpenLMISRepositoryImpl.prototype.search = search;
+        OpenLMISRepositoryImpl.prototype.get = get;
+
+        return OpenLMISRepositoryImpl;
+
+        function OpenLMISRepositoryImpl(url) {
+            this.resource = $resource(url + '/:id');
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf openlmis-repository.OpenLMISRepositoryImpl
+         * @name get
+         *
+         * @description
+         * Retrieves a shipment with the given ID.
+         *
+         * @param   {string}    id  the ID of the shipment
+         * @return  {Promise}       the promise resolving to server response, rejects if ID is not
+         *                          given or if the request fails
+         */
+        function get(id) {
+            if (id) {
+                return this.resource.get({
+                    id: id
+                }).$promise;
+            }
+            return $q.reject();
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf openlmis-repository.OpenLMISRepositoryImpl
+         * @name search
+         *
+         * @description
+         * Return the response of the GET request. Passes the given object as request parameters.
+         *
+         * @param   {Object}    params  the map of request parameters
+         * @return  {Promise}           the promise resolving to the server response
+         */
+        function search(params) {
+            return this.resource.get(params).$promise;
+        }
+
+    }
+
+})();
---
+++ src/openlmis-repository/openlmis-repository-impl.spec.js
@@ -0,0 +1,153 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('OpenLMISRepositoryImpl', function() {
+
+    var BASE_URL = 'some.url/com';
+
+    var openLMISRepositoryImpl, OpenLMISRepositoryImpl, $httpBackend, $rootScope, PageDataBuilder,
+        page;
+
+    beforeEach(function() {
+        module('openlmis-config');
+        module('openlmis-pagination');
+        module('openlmis-repository');
+
+        inject(function($injector) {
+            $httpBackend = $injector.get('$httpBackend');
+            $rootScope = $injector.get('$rootScope');
+            OpenLMISRepositoryImpl = $injector.get('OpenLMISRepositoryImpl');
+            PageDataBuilder = $injector.get('PageDataBuilder');
+        });
+
+        openLMISRepositoryImpl = new OpenLMISRepositoryImpl(BASE_URL);
+
+        page = new PageDataBuilder().build();
+    });
+
+    describe('search', function() {
+
+        it('should return server response on successful request', function() {
+            $httpBackend
+            .expectGET(BASE_URL + '?ids=id-one&ids=id-two&page=0&paramOne=valueOne&size=3')
+            .respond(200, page);
+
+            var result;
+            openLMISRepositoryImpl.search({
+                ids: ['id-one', 'id-two'],
+                paramOne: 'valueOne',
+                page: '0',
+                size: '3'
+            })
+            .then(function(page) {
+                result = page;
+            });
+            $httpBackend.flush();
+
+            expect(angular.toJson(result)).toEqual(angular.toJson(page));
+        });
+
+        it('should accept undefine as argument', function() {
+            $httpBackend
+            .expectGET(BASE_URL)
+            .respond(200, page);
+
+            var result;
+            openLMISRepositoryImpl.search()
+            .then(function(page) {
+                result = page;
+            });
+            $httpBackend.flush();
+
+            expect(angular.toJson(result)).toEqual(angular.toJson(page));
+        });
+
+        it('should reject if request was not successful', function() {
+            $httpBackend
+            .expectGET(BASE_URL)
+            .respond(400);
+
+            var rejected;
+            openLMISRepositoryImpl.search()
+            .catch(function() {
+                rejected = true;
+            });
+            $httpBackend.flush();
+
+            expect(rejected).toBe(true);
+        });
+
+    });
+
+    describe('get', function() {
+
+        var response;
+
+        beforeEach(function() {
+            response = {
+                id: 'some-id',
+                some: 'test-object'
+            };
+        });
+
+        it('should resolve to server response on successful request', function() {
+            $httpBackend
+            .expectGET(BASE_URL + '/' + response.id)
+            .respond(200, response);
+
+            var result;
+            openLMISRepositoryImpl.get(response.id)
+            .then(function(response) {
+                result = response;
+            });
+            $httpBackend.flush();
+
+            expect(angular.toJson(result)).toEqual(angular.toJson(response));
+        });
+
+        it('should reject on failed request', function() {
+            $httpBackend
+            .expectGET(BASE_URL + '/' + response.id)
+            .respond(400);
+
+            var rejected;
+            openLMISRepositoryImpl.get(response.id)
+            .catch(function() {
+                rejected = true;
+            });
+            $httpBackend.flush();
+
+            expect(rejected).toEqual(true);
+        });
+
+        it('should reject if null was given', function() {
+            var rejected;
+            openLMISRepositoryImpl.get()
+            .catch(function() {
+                rejected = true;
+            });
+            $rootScope.$apply();
+
+            expect(rejected).toBe(true);
+        });
+
+    });
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingRequest();
+        $httpBackend.verifyNoOutstandingExpectation();
+    });
+
+});
