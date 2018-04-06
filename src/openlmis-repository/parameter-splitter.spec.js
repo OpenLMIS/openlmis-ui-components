eb2b8c444373240d998d6c47a58d1ea0e4edff26
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-04-06 10:57:41 +0200

    OLMIS-4515: Removed ddescribe

--- src/openlmis-repository/parameter-splitter.spec.js
+++ src/openlmis-repository/parameter-splitter.spec.js
@@ -13,7 +13,7 @@
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-ddescribe('ParameterSplitter', function() {
+describe('ParameterSplitter', function() {
 
     var ParameterSplitter, uri, maxUriLength, params;
 
