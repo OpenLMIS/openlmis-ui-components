1ea1fabc0ef4d8da9f157a8d420f2cf6e83432c5
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-06-28 17:43:11 +0200

    OLMIS-4935: removed accidently copied docs

--- src/openlmis-uuid/uuid-generator.js
+++ src/openlmis-uuid/uuid-generator.js
@@ -61,20 +61,6 @@
                 d += performance.now();
             }
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-         * @methodOf openlmis-uuid.UuidGenerator
-         * @name generate
-         *
-         * @description
-         * Generates string with UUID v4.
-         * 
-         * @returns {String} UUIDv4 string
-         */
-        function generate() {
-            var d = new Date().getTime();
-            if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
-                d += performance.now();
-            }
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = (d + Math.random() * 16) % 16 | 0;
                 d = Math.floor(d / 16);
                 return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
