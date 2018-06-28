3ae38f040935aca3bcdf8af74c738b3954304a92
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-06-28 17:40:39 +0200

    OLMIS-4935: removed unused variable, removed trailing spaces

--- src/openlmis-uuid/uuid-generator.js
+++ src/openlmis-uuid/uuid-generator.js
@@ -28,7 +28,7 @@
         .module('openlmis-uuid')
         .factory('UuidGenerator', UuidGenerator);
 
-    function UuidGenerator($q) {
+    function UuidGenerator() {
 
         UuidGenerator.prototype.generate = generate;
 
@@ -55,12 +55,26 @@
          * 
          * @returns {String} UUIDv4 string
          */
-        function generate() {   
+        function generate() {
             var d = new Date().getTime();
-            if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+            if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
                 d += performance.now();
             }
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+         * @methodOf openlmis-uuid.UuidGenerator
+         * @name generate
+         *
+         * @description
+         * Generates string with UUID v4.
+         * 
+         * @returns {String} UUIDv4 string
+         */
+        function generate() {
+            var d = new Date().getTime();
+            if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+                d += performance.now();
+            }
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = (d + Math.random() * 16) % 16 | 0;
                 d = Math.floor(d / 16);
                 return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
