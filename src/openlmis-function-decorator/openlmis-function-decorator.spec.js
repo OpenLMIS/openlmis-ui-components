0861a4ead85bc391ae86e9719f783792928fd8d4
Nikodem Graczewski <ngraczewski@soldevelo.com>
2019-03-21 12:02:22 +0100

    OLMIS-6020: Removed ddescribe

--- src/openlmis-function-decorator/openlmis-function-decorator.spec.js
+++ src/openlmis-function-decorator/openlmis-function-decorator.spec.js
@@ -13,7 +13,7 @@
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-ddescribe('FunctionDecorator', function() {
+describe('FunctionDecorator', function() {
 
     beforeEach(function() {
         module('openlmis-function-decorator');
